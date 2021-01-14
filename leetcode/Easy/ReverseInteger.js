c8687371c3ff9b2eafe6ac54287d10d381d44072
eslamelkholy <eslamelkholy5325762@gmail.com>
2021-01-14 18:48:40 +0200

    Added Reverse Integer Within Edge Cases

--- leetcode/Easy/ReverseInteger.js
+++ leetcode/Easy/ReverseInteger.js
@@ -11,27 +11,25 @@
  */
 
 // Mathematical but for Positive Numbers Only
-var reverse2 = function(num) {
+var reverse2 = function (num) {
   let reversedInteger = 0;
   let rem = 0;
   while (num > 0) {
     reversedInteger *= 10;
-    rem = num % 10
-    num = (num - rem) / 10; 
-    reversedInteger += rem
-  }  
+    rem = num % 10;
+    num = (num - rem) / 10;
+    reversedInteger += rem;
+  }
   return reversedInteger;
 };
 
 // Simple Stright Forward Approach
-const reverse = (num)   => {
-    const MAX_SAFE_INTEGER= Math.pow(2, 31);
-    const MIN_SAFE_INTEGER = -MAX_SAFE_INTEGER - 1;
-    const reversedNumber = parseInt(num.toString().split("").reverse().join(""))* Math.sign(num);
-    return reversedNumber >= MIN_SAFE_INTEGER && reversedNumber <= MAX_SAFE_INTEGER ? reversedNumber: 0; 
-}
-
-
+const reverse = (num) => {
+  const MAX_SAFE_INTEGER = Math.pow(2, 31);
+  const MIN_SAFE_INTEGER = -MAX_SAFE_INTEGER - 1;
+  const reversedNumber = parseInt(num.toString().split("").reverse().join("")) * Math.sign(num);
+  return reversedNumber >= MIN_SAFE_INTEGER && reversedNumber <= MAX_SAFE_INTEGER ? reversedNumber : 0;
+};
 
 // console.log(reverse1(1534236469));
 // console.log(reverse1(-123));
