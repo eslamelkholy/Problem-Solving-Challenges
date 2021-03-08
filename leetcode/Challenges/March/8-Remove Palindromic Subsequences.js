9af4b71d42a268dfc4468a325018f9db0c9de196
eslamelkholy <eslamelkholy5325762@gmail.com>
2021-03-08 21:31:32 +0200

    Day8 Problem Palindromic

---
+++ leetcode/Challenges/March/8-Remove Palindromic Subsequences.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var removePalindromeSub = function (s) {
+  if (s.length === 0) return 0;
+  let reversedElement = '';
+  for (let i = 0; i < s.length - 1; i++) if (s[i] !== s[s.length - 1 - i]) return 2;
+  return 1;
+};
+
+// console.log(removePalindromeSub('ababa'));
+console.log(removePalindromeSub('abb'));
