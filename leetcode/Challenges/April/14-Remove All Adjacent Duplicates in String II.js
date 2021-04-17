95aad11cbfe47df86b87cf536ac5f141f4cde997
eslamelkholy <eslamelkholy5325762@gmail.com>
2021-04-17 17:12:17 +0200

    Removing all Adjacent Duplicates String

--- leetcode/Challenges/April/14-Remove All Adjacent Duplicates in String II.js
+++ leetcode/Challenges/April/14-Remove All Adjacent Duplicates in String II.js
@@ -4,17 +4,19 @@
  * @return {string}
  */
 
-var removeDuplicates = function (S, K) {
-  let stackChar = S.split('');
-  let stack = [0];
-  let i;
-  let j;
-  for (i = 1, j = 1; j < S.length; stackChar[++i] = stackChar[++j]) {
-    if (stackChar[i] !== stackChar[i - 1]) stack.push(i);
-    else if (i - stack[stack.length - 1] + 1 === K) i = stack.pop() - 1;
-  }
-  return stackChar.slice(0, i + 1).join('');
+console.log(removeDuplicates('pbbcggttciiippooaais', 2));
+var removeDuplicates = function (s, k) {
+  const stringFrequent = {};
+  let result = '';
+  for (const char of s)
+    if (stringFrequent[char]) {
+      stringFrequent[char]++;
+      if (stringFrequent[char] === k) delete stringFrequent[char];
+    } else stringFrequent[char] = 1;
+  for (const key in stringFrequent) result = result.concat(...new Array(stringFrequent[key]).fill(key));
+  return result;
 };
+
 console.log(removeDuplicates('yfttttfbbbbnnnnffbgffffgbbbbgssssgthyyyy', 4));
 // console.log(removeDuplicates('pbbcggttciiippooaais', 2));
 // console.log(removeDuplicates('deeedbbcccbdaa', 3));
