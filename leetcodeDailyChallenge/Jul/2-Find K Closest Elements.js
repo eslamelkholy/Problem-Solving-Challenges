40573e571e0d78f65d8216bcb09d74a1d5e91923
eslamelkholy <eslamelkholy5325762@gmail.com>
2021-07-02 16:19:18 +0200

    Adding Find K Closest Element Solution Using Two Pointers

---
+++ leetcodeDailyChallenge/Jul/2-Find K Closest Elements.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} arr
+ * @param {number} k
+ * @param {number} x
+ * @return {number[]}
+ */
+var findClosestElements = function (arr, k, x) {
+  let leftPointer = 0;
+  let rightPointer = arr.length - 1;
+  while (rightPointer - leftPointer >= k) {
+    if (x - arr[leftPointer] <= arr[rightPointer] - x) rightPointer--;
+    else leftPointer++;
+  }
+  return arr.slice(leftPointer, rightPointer + 1);
+};
+
+// console.log(findClosestElements([1, 2, 3, 4, 5], 4, -1)); // [1,2,3,4]
+// console.log(findClosestElements([1, 2, 3, 4, 5], 4, 3)); // [1,2,3,4]
+console.log(findClosestElements([1, 1, 1, 10, 10, 10], 1, 9)); // [1,2,3,4]
