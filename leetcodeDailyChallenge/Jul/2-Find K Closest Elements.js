8d800d050ec82034ef0241e2b23654e361af8bbb
eslamelkholy <eslamelkholy5325762@gmail.com>
2021-07-02 16:22:55 +0200

    Adding Some Documentation for the Solution

--- leetcodeDailyChallenge/Jul/2-Find K Closest Elements.js
+++ leetcodeDailyChallenge/Jul/2-Find K Closest Elements.js
@@ -8,7 +8,8 @@ var findClosestElements = function (arr, k, x) {
   let leftPointer = 0;
   let rightPointer = arr.length - 1;
   while (rightPointer - leftPointer >= k) {
-    if (x - arr[leftPointer] <= arr[rightPointer] - x) rightPointer--;
+    // a-x <= b-x (Comparison Law at the Question)
+    if (Math.abs(arr[leftPointer] - x) <= Math.abs(arr[rightPointer] - x)) rightPointer--;
     else leftPointer++;
   }
   return arr.slice(leftPointer, rightPointer + 1);
