dc9a4cb42970ca70aa25d673d1a5f6936c55e1af
eslamelkholy <eslamelkholy5325762@gmail.com>
2021-07-06 14:38:00 +0200

    Added Day6 Challenge Reduce Array Size to Half

---
+++ leetcodeDailyChallenge/Jul/6-Reduce Array Size to The Half.js
@@ -0,0 +1,12 @@
+var minSetSize = function (arr) {
+  const numFreq = {};
+  let count = 0;
+  for (let i = 0; i < arr.length; i++) numFreq[arr[i]] ? numFreq[arr[i]]++ : (numFreq[arr[i]] = 1);
+
+  const sortedIntegerFeq = Object.values(numFreq).sort((a, b) => b - a);
+
+  for (let i = 0; i < sortedIntegerFeq.length; i++) {
+    count += sortedIntegerFeq[i];
+    if (count >= arr.length / 2) return i + 1;
+  }
+};
