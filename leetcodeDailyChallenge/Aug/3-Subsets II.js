1fbdc1e4ad65ee573a104489d05d736a5bd239c2
eslamelkholy <eslamelkholy5325762@gmail.com>
2021-08-03 21:12:42 +0200

    Added SubSet || Solution Without Duplication Using DFS

---
+++ leetcodeDailyChallenge/Aug/3-Subsets II.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var subsetsWithDup = function (nums) {
+  var subSet = [];
+
+  dfs(nums, subSet);
+
+  return subSet;
+};
+
+var dfs = function (nums, subSet, tmpArray = [], start = 0) {
+  subSet.push(Array.from(tmpArray));
+  if (start === nums.length) return;
+
+  for (var i = start; i < nums.length; i++) {
+    if (i > start && nums[i] == nums[i - 1]) continue;
+
+    tmpArray.push(nums[i]);
+    dfs(nums, subSet, tmpArray, i + 1);
+    tmpArray.pop();
+  }
+};
+
+console.log(subsetsWithDup([1, 2, 2]));
