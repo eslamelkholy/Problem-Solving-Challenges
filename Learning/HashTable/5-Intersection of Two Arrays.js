2f812f00ab46dd0fb0c12d943e11c8c1c47ac480
eslamelkholy <eslamelkholy5325762@gmail.com>
2021-08-22 17:52:36 +0200

    Added Intersection Of Two Numbers Solution Using Two Hash Set

---
+++ Learning/HashTable/5-Intersection of Two Arrays.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersection = function (nums1, nums2) {
+  const set1 = new Set();
+  const set2 = new Set();
+  for (const num of nums1) set1.add(num);
+  for (const num of nums2) set2.add(num);
+  return set1.size > set2.size ? findIntersection(set1, set2) : findIntersection(set2, set1);
+};
+
+const findIntersection = (set1, set2) => {
+  const result = [];
+  for (const val of set1) if (set2.has(val)) result.push(val);
+  return result;
+};
+
+console.log(intersection([1, 2, 2, 1], [2, 2]));
