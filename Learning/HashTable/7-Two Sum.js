c036f58cb062aaeca8b87278a1ebf6ecdd7aa220
eslamelkholy <eslamelkholy5325762@gmail.com>
2021-08-23 23:28:09 +0200

    TWO Sum Using O(N) Time Complexity

---
+++ Learning/HashTable/7-Two Sum.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+  const numMap = new Map();
+  const map2 = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    numMap.set(i, nums[i]);
+    map2.set(nums[i], i);
+  }
+
+  for (const [index, num] of numMap) {
+    if (map2.has(target - num) && index !== map2.get(target - num)) return [index, map2.get(target - num)];
+  }
+};
+
+console.log(twoSum([2, 7, 11, 15], 9));
+// console.log(twoSum([3, 3], 6));
+console.log(twoSum([3, 2, 4], 6));
