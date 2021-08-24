e9fc5730612e327b2656fdb8af700b4c20013c9b
eslamelkholy <eslamelkholy5325762@gmail.com>
2021-08-24 21:59:27 +0200

    Contains Duplicate || Using Map

---
+++ Learning/HashTable/12-Contains Duplicate II.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsNearbyDuplicate = function (nums, k) {
+  const map = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    if (i - map.get(nums[i]) <= k) return true;
+
+    map.set(nums[i], i);
+  }
+  return false;
+};
+
+console.log(containsNearbyDuplicate([1, 2, 3, 1], 3));
+console.log(containsNearbyDuplicate([1, 0, 1, 1], 1));
+console.log(containsNearbyDuplicate([1, 2, 3, 1, 2, 3], 2));
