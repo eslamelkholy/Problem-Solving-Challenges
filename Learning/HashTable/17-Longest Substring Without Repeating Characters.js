b6cb3c4c9045ea4228df5d13bf0d9c119b1cac15
eslamelkholy <eslamelkholy5325762@gmail.com>
2021-08-26 21:51:18 +0200

    Longest SubString WIthout Repeating Characters

--- Learning/HashTable/17-Longest Substring Without Repeating Characters.js
+++ Learning/HashTable/17-Longest Substring Without Repeating Characters.js
@@ -2,4 +2,24 @@
  * @param {string} s
  * @return {number}
  */
-var lengthOfLongestSubstring = function (s) {};
+var lengthOfLongestSubstring = function (s) {
+  const subStrMap = {};
+  let result = 0;
+  for (let i = 0; i < s.length; i++) {
+    let currentSub = s[i];
+    for (let j = i + 1; j < s.length; j++) currentSub.includes(s[j]) ? (j = s.length) : (currentSub += s[j]);
+
+    subStrMap[currentSub] = currentSub.length;
+    if (currentSub.length > result) result = currentSub.length;
+    currentSub = '';
+  }
+
+  return result;
+};
+
+console.log(lengthOfLongestSubstring('abcabcbb'));
+console.log(lengthOfLongestSubstring('bbbbb'));
+console.log(lengthOfLongestSubstring('pwwkew'));
+console.log(lengthOfLongestSubstring(''));
+console.log(lengthOfLongestSubstring('aab'));
+console.log(lengthOfLongestSubstring('dvdf'));
