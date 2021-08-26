43932ce3a33270487d3efd38348f96b91c99eca8
eslamelkholy <eslamelkholy5325762@gmail.com>
2021-08-26 22:25:49 +0200

    Top K Frequent Elements Solution

---
+++ Learning/HashTable/19-Top K Frequent Elements.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+function compare(a, b) {
+  if (a.freq < b.freq) {
+    return 1;
+  }
+  if (a.freq > b.freq) {
+    return -1;
+  }
+  return 0;
+}
+var topKFrequent = function (nums, k) {
+  const numsMap = {};
+  const result = [];
+  const freqArr = [];
+  for (const num of nums) numsMap[num] ? numsMap[num]++ : (numsMap[num] = 1);
+
+  for (const key in numsMap) freqArr.push({ freq: numsMap[key], val: key });
+
+  freqArr.sort((a, b) => b.freq - a.freq);
+  for (let i = 1; i < k + 1; i++) result.push(parseInt(freqArr[i - 1].val));
+
+  return result;
+};
+
+console.log(topKFrequent([1, 1, 1, 2, 2, 3], 2));
+// console.log(topKFrequent([1], 1));
+console.log(topKFrequent([1, 2], 2));
