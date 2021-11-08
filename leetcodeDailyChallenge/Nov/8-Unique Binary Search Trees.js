8bd942c50e0389076a007f0a64cb8c57162acd35
eslamelkholy <eslamelkholy5325762@gmail.com>
2021-11-08 22:27:33 +0200

    Day8 Problem Unique Binary Search Tree Solution

---
+++ leetcodeDailyChallenge/Nov/8-Unique Binary Search Trees.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var numTrees = function (n) {
+  const dp = new Array(n + 1).fill(0);
+  dp[1] = dp[0] = 1;
+
+  for (let i = 2; i <= n; ++i) {
+    for (let j = 0; j < i; ++j) {
+      dp[i] += dp[j] * dp[i - 1 - j];
+    }
+  }
+  return dp[n];
+};
+
+console.log(numTrees(3));
