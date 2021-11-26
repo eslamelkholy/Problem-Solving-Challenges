6d9678090883880c615a56fd4420917bd8e1d9d1
eslamelkholy <eslamelkholy5325762@gmail.com>
2021-11-26 16:41:34 +0200

    Binary Tree Level Order Using BFS

---
+++ CodingPreparation/binaryTree/Binary Tree Level Order Traversal.js
@@ -0,0 +1,19 @@
+var levelOrder = function (root) {
+  if (!root) return [];
+  const queue = [root];
+  const result = [];
+  while (queue.length > 0) {
+    const levelValues = [];
+    const n = queue.length; // Save Queue Length  HERE
+    for (let i = 0; i < n; i++) {
+      const node = queue.shift();
+      levelValues.push(node.val);
+
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+    result.push(levelValues);
+  }
+
+  return result;
+};
