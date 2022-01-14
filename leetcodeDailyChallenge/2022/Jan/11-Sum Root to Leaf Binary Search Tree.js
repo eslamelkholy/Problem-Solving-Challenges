adeb5f184207ec73c9557f93d28bb0659d899efc
eslamelkholy <eslamelkholy5325762@gmail.com>
2022-01-14 14:40:47 +0200

    Day11 Sum Root to Leaf Of Binary Search Tree

---
+++ leetcodeDailyChallenge/2022/Jan/11-Sum Root to Leaf Binary Search Tree.js
@@ -0,0 +1,16 @@
+var sumRootToLeaf = function (root) {
+  let sum = 0;
+  const dfs = (root, prev = "") => {
+    if (!root) return;
+    prev += root.val;
+
+    if (!root.left && !root.right) {
+      sum += parseInt(prev, 2);
+    }
+
+    dfs(root.left, prev);
+    dfs(root.right, prev);
+  };
+  dfs(root);
+  return sum;
+};
