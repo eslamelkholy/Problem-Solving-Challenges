861a5d8908302c193b0610dc564f823a446fa093
eslamelkholy <55209776+eslamelkholy@users.noreply.github.com>
2022-02-06 18:53:53 +0200

    Day6 Remove Duplicates from Sorted Array II

---
+++ leetcodeDailyChallenge/2022/Feb/5-Remove Duplicates from Sorted Array II.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+ var removeDuplicates = function(nums) {
+  let prevElement = nums[0];
+  for(let i = 1; i < nums.length ; i++) {
+    if(prevElement === nums[i] && nums[i] === nums[i+1] ) {
+      for (let j = i ; j < nums.length; j++) {
+        if(nums[j] === prevElement) {
+          nums.splice(j, 1);
+          i--;
+          break;
+        }
+      }
+    }
+    prevElement = nums[i];
+  }
+  return nums;
+};
+
+console.log(removeDuplicates([1,1,1,2,2,3]))
+console.log(removeDuplicates([0,0,1,1,1,1,2,3,3]))
+console.log(removeDuplicates([0,0,0,0,0]))
