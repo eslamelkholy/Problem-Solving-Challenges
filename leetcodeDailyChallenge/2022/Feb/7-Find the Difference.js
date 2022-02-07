7ca218fb200edbeef528dad3466c15bcf46f9a19
eslamelkholy <55209776+eslamelkholy@users.noreply.github.com>
2022-02-07 13:26:39 +0200

    Find The Difference Using One HashMap Only

--- leetcodeDailyChallenge/2022/Feb/7-Find the Difference.js
+++ leetcodeDailyChallenge/2022/Feb/7-Find the Difference.js
@@ -25,6 +25,22 @@ var findTheDifference = function(s, t) {
   return -1;
 };
 
+var findTheDifference = function(s, t) {
+  const firstStringMap = {};
+
+  for (const char of s) {
+    firstStringMap[char] ? firstStringMap[char]++: firstStringMap[char] = 1;
+  }
+
+  for (const char of t) {
+    if(firstStringMap[char]) {
+        firstStringMap[char]--;
+    }else {
+        return char;
+    }
+  }
+};
+
 console.log(findTheDifference("abcd", "abcde"))
 console.log(findTheDifference("", "t"))
 console.log(findTheDifference("aa", "a"))
