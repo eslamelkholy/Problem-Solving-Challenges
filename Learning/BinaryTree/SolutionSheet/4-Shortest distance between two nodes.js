4d48e0fb9efee4e38f778ee6d2075132bb9f972a
eslamelkholy <eslamelkholy5325762@gmail.com>
2022-03-15 21:39:20 +0200

    Shortest Distance Between two Nodes in Binary Search Tree

---
+++ Learning/BinaryTree/SolutionSheet/4-Shortest distance between two nodes.js
@@ -0,0 +1,85 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} target
+ * @param {number} k
+ * @return {number[]}
+ */
+var minDiffInBST = function (root, a = 9, b = 25) {
+  let minDiff = 0;
+
+  // Takes LCA
+  const sumDistance = (root, distance) => {
+    if (!root) return;
+
+    distance += 1;
+    if (root.val === a || root.val === b) {
+      minDiff += distance;
+    }
+
+    sumDistance(root.left, distance);
+    sumDistance(root.right, distance);
+  };
+  const dfs = (root) => {
+    if (!root) return;
+
+    if (a > root.val && b > root.val) {
+      return dfs(root.right);
+    }
+
+    if (a < root.val && b < root.val) {
+      return dfs(root.left);
+    }
+    sumDistance(root.left, 0);
+    sumDistance(root.right, 0);
+  };
+
+  dfs(root);
+
+  return minDiff;
+};
+
+var minDiffInBST = function (root, a = 3, b = 9) {
+  let minDiff = 0;
+
+  // Takes LCA
+  const sumDistance = (root, distance) => {
+    if (!root) return;
+
+    distance += 1;
+    if (root.val === a || root.val === b) {
+      minDiff += distance;
+    }
+
+    sumDistance(root.left, distance);
+    sumDistance(root.right, distance);
+  };
+  const dfs = (root) => {
+    if (!root) return;
+
+    if (a > root.val && b > root.val) {
+      return dfs(root.right);
+    }
+
+    if (a < root.val && b < root.val) {
+      return dfs(root.left);
+    }
+    sumDistance(root.left, 0);
+    sumDistance(root.right, 0);
+  };
+
+  dfs(root);
+
+  return minDiff;
+};
+
+// Use this Data Samples for test
+// https://leetcode.com/problems/minimum-distance-between-bst-nodes/
+
+// [5,2,12,1,3,9,21,null,null, null,null,null,null, 19,25]
