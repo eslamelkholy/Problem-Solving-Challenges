f9b26d2e329da3d01bb8feb0b736cdb751eea429
eslamelkholy <eslamelkholy5325762@gmail.com>
2022-03-16 13:24:48 +0200

    Solving Fibonacci Number Using DP

---
+++ Learning/Mixed/SolutionSheet/1-Fibonacci Number.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var fib = function (n) {
+  const dp = new Array(n + 1).fill(0);
+  dp[0] = 0;
+  dp[1] = 1;
+  for (let i = 2; i < n + 1; i++) {
+    dp[i] = dp[i - 1] + dp[i - 2];
+  }
+
+  return dp[n];
+};
+
+console.log(fib(4)); // 4
