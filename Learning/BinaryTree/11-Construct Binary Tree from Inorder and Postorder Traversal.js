308272474110a891042f9dc091b35dc9c054bd9c
eslamelkholy <eslamelkholy5325762@gmail.com>
2022-04-02 23:08:45 +0200

    Build Tree Version2 Optimized Using Hash Map

--- Learning/BinaryTree/11-Construct Binary Tree from Inorder and Postorder Traversal.js
+++ Learning/BinaryTree/11-Construct Binary Tree from Inorder and Postorder Traversal.js
@@ -8,28 +8,50 @@ function TreeNode(val, left, right) {
  * @param {number[]} postorder
  * @return {TreeNode}
  */
-var buildTree = function (inorder, postorder) {
-  let NodesMap = {};
-  let startPointer = 0;
-  let endPointer = inorder.length - 1;
+// var buildTree = function (inorder, postorder) {
+//   let NodesMap = {};
+//   let startPointer = 0;
+//   let endPointer = inorder.length - 1;
 
-  inorder.forEach((v, i) => (NodesMap[v] = i));
+//   inorder.forEach((v, i) => (NodesMap[v] = i));
+
+//   const construct = (start, end) => {
+//     if (start > end) return null;
+
+//     const rootVal = postorder.pop();
+//     const root = new TreeNode(rootVal);
+//     const index = NodesMap[rootVal];
+
+//     root.right = construct(index + 1, end);
+//     root.left = construct(start, index - 1);
+
+//     return root;
+//   };
+
+//   return construct(startPointer, endPointer);
+// };
+
+var buildTree1 = function (inorder, postorder) {
+  const nodesMap = {};
+
+  inorder.forEach((val, index) => (nodesMap[val] = index));
+  let leftPointer = 0;
+  let rightPointer = inorder.length - 1;
 
   const construct = (start, end) => {
     if (start > end) return null;
 
-    const rootVal = postorder.pop();
-    const root = new TreeNode(rootVal);
-    const index = NodesMap[rootVal];
+    const nodeVal = postorder.pop();
+    const nodeIndex = nodesMap[nodeVal];
 
-    root.right = construct(index + 1, end);
-    root.left = construct(start, index - 1);
+    const root = new TreeNode(nodeVal);
+    root.right = construct(nodeIndex + 1, end);
+    root.left = construct(start, nodeIndex - 1);
 
     return root;
   };
 
-  return construct(startPointer, endPointer);
+  return construct(leftPointer, rightPointer);
 };
-
-console.log(buildTree([9, 3, 15, 20, 7], [9, 15, 7, 20, 3]));
-console.log(buildTree([-1], [-1]));
+console.log(buildTree1([9, 3, 15, 20, 7], [9, 15, 7, 20, 3]));
+console.log(buildTree1([-1], [-1]));
