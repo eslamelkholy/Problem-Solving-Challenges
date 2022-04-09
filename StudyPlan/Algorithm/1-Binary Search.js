36b2f9c33a2ac8cfc9a9869aa31843a7445515e6
eslamelkholy <eslamelkholy5325762@gmail.com>
2022-04-09 19:47:23 +0200

    Binary Search Algorithm Well Tested

---
+++ StudyPlan/Algorithm/1-Binary Search.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function (nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    const current = nums[mid];
+
+    if (current === target) return mid;
+
+    if (current > target) {
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
+  }
+
+  return -1;
+};
+
+console.log(search([-1, 0, 3, 5, 9, 12], 9));
+console.log(search([-1, 0, 3, 5, 9, 12], 2));
+console.log(search([5], 5));
+console.log(search([2, 5], 5));
