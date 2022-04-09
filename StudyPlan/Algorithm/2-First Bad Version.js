05063168fcc4ef9ee15eae455a148009edac2781
eslamelkholy <eslamelkholy5325762@gmail.com>
2022-04-09 22:07:33 +0200

    First Bad Version Using Binary Search

---
+++ StudyPlan/Algorithm/2-First Bad Version.js
@@ -0,0 +1,37 @@
+/**
+ * Definition for isBadVersion()
+ *
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+
+/**
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+var solution = function (isBadVersion) {
+  /**
+   * @param {integer} n Total versions
+   * @return {integer} The first bad version
+   */
+  return function (n) {
+    let left = 0;
+    let right = n;
+
+    while (left < right) {
+      const mid = Math.floor((left + right) / 2);
+      const isBad = isBadVersion(mid);
+
+      if (isBad) {
+        right = mid;
+      } else {
+        left = mid + 1;
+      }
+    }
+
+    return left;
+  };
+};
