2d2e0e34cf537d627ce624a695f01997dd3de0d0
eslamelkholy <eslamelkholy5325762@gmail.com>
2022-04-10 19:26:49 +0200

    Square of Sorted Array Using Two Pointers

--- StudyPlan/Algorithm/4-Squares of a Sorted Array.js
+++ StudyPlan/Algorithm/4-Squares of a Sorted Array.js
@@ -2,13 +2,33 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var sortedSquares = function (nums) {
+var sortedSquaresStraighForward = function (nums) {
   const result = [];
   for (let i = 0; i < nums.length; i++) {
     result.push(Math.pow(nums[i], 2));
   }
 
   return result.sort((a, b) => a - b);
+};
+
+var sortedSquares = function (nums) {
+  const result = [];
+  let leftPointer = 0;
+  let rightPointer = nums.length - 1;
+
+  while (leftPointer <= rightPointer) {
+    const leftPow = Math.pow(nums[leftPointer], 2);
+    const rightPow = Math.pow(nums[rightPointer], 2);
+    if (leftPow > rightPow) {
+      result.push(leftPow);
+      leftPointer++;
+    } else {
+      result.push(rightPow);
+      rightPointer--;
+    }
+  }
+
+  return result.reverse();
 };
 
 console.log(sortedSquares([-4, -1, 0, 3, 10]));
