7aa556272d94a97fd8aa99b745ab812406afe0b5
eslamelkholy <eslamelkholy5325762@gmail.com>
2022-04-10 19:49:57 +0200

    Rotate Array Using Two Pointers

---
+++ StudyPlan/Algorithm/5-Rotate Array.js
@@ -0,0 +1,42 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotateTimeLimitExceed = function (nums, k) {
+  for (let i = 0; i < k; i++) {
+    nums.unshift(nums.pop());
+  }
+};
+
+// T.C: O(N)
+// S.C: O(1)
+/*
+  Solution:
+  1. Reverse the entire array.
+  2. Reverse the first k elements.
+  3. Reverse the rest.
+*/
+var rotate = function (nums, k) {
+  if (nums === null || nums.length === 0) {
+    return;
+  }
+  k = k % nums.length;
+  if (k === 0) {
+    return;
+  }
+  nums.reverse();
+  reverse(nums, 0, k - 1);
+  reverse(nums, k, nums.length - 1);
+};
+
+const reverse = (arr, start, end) => {
+  while (start < end) {
+    let temp = arr[start];
+    arr[start] = arr[end];
+    arr[end] = temp;
+    start++, end--;
+  }
+};
+
+console.log(rotate([1, 2, 3, 4, 5, 6, 7], 15));
