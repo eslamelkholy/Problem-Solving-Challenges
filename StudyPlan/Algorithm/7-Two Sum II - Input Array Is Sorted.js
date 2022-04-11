34258db9f0d7779d05d377db3f5874ac8a94d60f
eslamelkholy <eslamelkholy5325762@gmail.com>
2022-04-11 20:38:54 +0200

    Two SUM || Sorted Array Using Two Pointers

--- StudyPlan/Algorithm/7-Two Sum II - Input Array Is Sorted.js
+++ StudyPlan/Algorithm/7-Two Sum II - Input Array Is Sorted.js
@@ -7,7 +7,6 @@ var twoSum = function (numbers, target) {
   let leftPointer = 0;
   let rightPointer = numbers.length - 1;
   while (leftPointer < rightPointer) {
-    // console.log("STEPS COUNT");
     const sum = numbers[leftPointer] + numbers[rightPointer];
     if (sum === target) {
       return [leftPointer + 1, rightPointer + 1];
