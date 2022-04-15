f9345248353bf247dee4143209d727c8aeda175d
eslamelkholy <eslamelkholy5325762@gmail.com>
2022-04-15 19:24:50 +0200

    Trim Binary Search Tree Using JS

---
+++ leetcodeDailyChallenge/2022/April/15-Trim a Binary Search Tree.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} low
+ * @param {number} high
+ * @return {TreeNode}
+ */
+var trimBST = function (root, low, high) {
+  const traverse = (root) => {
+    if (root === null) return root;
+
+    if (root.val > high) return traverse(root.left);
+    if (root.val < low) return traverse(root.right);
+
+    root.left = traverse(root.left);
+    root.right = traverse(root.right);
+    return root;
+  };
+
+  return traverse(root);
+};
