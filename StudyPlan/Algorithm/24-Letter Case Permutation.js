4e70a7a879100b80642e41c02094f424b1090e51
eslamelkholy <eslamelkholy5325762@gmail.com>
2022-04-19 22:06:47 +0200

    Letter Case Permutation

--- StudyPlan/Algorithm/24-Letter Case Permutation.js
+++ StudyPlan/Algorithm/24-Letter Case Permutation.js
@@ -2,9 +2,40 @@
  * @param {string} s
  * @return {string[]}
  */
+var letterCasePermutation1 = function (s) {
+  const result = [];
+  function dfs(i, build) {
+    if (i === s.length) {
+      return result.push(build);
+    }
+
+    if (!parseInt(s[i]) && parseInt(s[i]) !== 0) {
+      dfs(i + 1, build + s[i].toUpperCase());
+      dfs(i + 1, build + s[i].toLowerCase());
+    } else {
+      dfs(i + 1, build + s[i]);
+    }
+  }
+  dfs(0, "");
+  return result;
+};
+
 var letterCasePermutation = function (s) {
-  console.log(isNumeric("1"));
-  console.log(isNumeric("s"));
+  const result = [];
+  const startPosition = 0;
+  dfs(result, s, startPosition, "");
+  return result;
+};
+
+const dfs = (result, s, position, str) => {
+  if (str.length === s.length) return result.push(str);
+
+  if (isNumeric(s[position])) {
+    dfs(result, s, position + 1, str + s[position]);
+  } else {
+    dfs(result, s, position + 1, str + s[position].toUpperCase());
+    dfs(result, s, position + 1, str + s[position].toLowerCase());
+  }
 };
 
 function isNumeric(value) {
