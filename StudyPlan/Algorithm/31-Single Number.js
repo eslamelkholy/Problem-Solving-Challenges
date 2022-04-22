0e1d224f6b74fdf25df4e1817eeb3c1fe52d9ecd
eslamelkholy <eslamelkholy5325762@gmail.com>
2022-04-22 02:08:22 +0200

    Single Number Solution Using Hash Map

---
+++ StudyPlan/Algorithm/31-Single Number.js
@@ -0,0 +1,7 @@
+var singleNumber = function (nums) {
+  const map = new Map();
+  for (const num of nums)
+    map.has(num) ? map.set(num, map.get(num) + 1) : map.set(num, 1);
+
+  for (const [key, value] of map) if (value === 1) return key;
+};
