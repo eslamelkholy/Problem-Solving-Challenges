020077242c60241203efa823a7bae19c169731d1
eslamelkholy <eslamelkholy5325762@gmail.com>
2022-05-05 16:02:54 +0200

    Convert Solution TO ES6 Class

--- leetcodeDailyChallenge/2022/May/5-Implement Stack using Queues.js
+++ leetcodeDailyChallenge/2022/May/5-Implement Stack using Queues.js
@@ -1,40 +1,38 @@
-var MyStack = function () {
-  this.stack = [];
-  this.indexPointer = -1;
-};
-
-/**
- * @param {number} x
- * @return {void}
- */
-MyStack.prototype.push = function (x) {
-  this.stack.push(x);
-  this.indexPointer++;
-};
-
-/**
- * @return {number}
- */
-MyStack.prototype.pop = function () {
-  const result = this.stack[this.indexPointer];
-  this.stack.splice(this.indexPointer, 1);
-  this.indexPointer--;
-  return result;
-};
-
-/**
- * @return {number}
- */
-MyStack.prototype.top = function () {
-  return this.stack[this.indexPointer];
-};
-
-/**
- * @return {boolean}
- */
-MyStack.prototype.empty = function () {
-  return this.stack.length === 0;
-};
+class MyStack {
+  constructor() {
+    this.stack = [];
+    this.indexPointer = -1;
+  }
+  /**
+   * @param {number} x
+   * @return {void}
+   */
+  push(x) {
+    this.stack.push(x);
+    this.indexPointer++;
+  }
+  /**
+   * @return {number}
+   */
+  pop() {
+    const result = this.stack[this.indexPointer];
+    this.stack.splice(this.indexPointer, 1);
+    this.indexPointer--;
+    return result;
+  }
+  /**
+   * @return {number}
+   */
+  top() {
+    return this.stack[this.indexPointer];
+  }
+  /**
+   * @return {boolean}
+   */
+  empty() {
+    return this.stack.length === 0;
+  }
+}
 
 /**
  * Your MyStack object will be instantiated and called as such:
