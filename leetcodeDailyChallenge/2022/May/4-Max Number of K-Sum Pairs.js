daf61828f3952ff961f593e24991cb520f3536b3
eslamelkholy <eslamelkholy5325762@gmail.com>
2022-05-05 14:38:14 +0200

    Max Number of Operation Two Solutions

--- leetcodeDailyChallenge/2022/May/4-Max Number of K-Sum Pairs.js
+++ leetcodeDailyChallenge/2022/May/4-Max Number of K-Sum Pairs.js
@@ -4,6 +4,21 @@
  * @return {number}
  */
 var maxOperations = function (nums, k) {
+  let counter = 0;
+  const numMap = {};
+  for (let i = 0; i < nums.length; i++) {
+    const num = nums[i];
+    if (numMap[k - num + '']) {
+      counter++;
+      numMap[k - num + '']--;
+    } else {
+      numMap[num] = numMap[num] + 1 || 1;
+    }
+  }
+  return counter;
+};
+
+var maxOperations = function (nums, k) {
   const numMap = new Map();
   let counter = 0;
 
@@ -16,5 +31,7 @@ var maxOperations = function (nums, k) {
   }
   return counter;
 };
-console.log(maxOperations([1, 2, 3, 4], 5)); // 2
+
 console.log(maxOperations([3, 1, 3, 4, 3], 6)); // 1
+console.log(maxOperations([1, 2, 3, 4], 5)); // 2
+console.log(maxOperations([2, 5, 4, 4, 1, 3, 4, 4, 1, 4, 4, 1, 2, 1, 2, 2, 3, 2, 4, 2], 3)); // 4
