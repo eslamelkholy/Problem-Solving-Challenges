1344bb5eaeb9c84b3bf9053f40d6fab7c334f29e
eslamelkholy <eslamelkholy5325762@gmail.com>
2022-05-07 20:01:44 +0200

    Find Minimum in Rotated SA Using Binary Search

--- StudyPlan/Algorithm2/4-Find Minimum in Rotated Sorted Array.js
+++ StudyPlan/Algorithm2/4-Find Minimum in Rotated Sorted Array.js
@@ -2,6 +2,44 @@
  * @param {number[]} nums
  * @return {number}
  */
-var findMin = function (nums) {
+var findMin1 = function (nums) {
   return Math.min(...nums);
 };
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMin = function (nums) {
+  if (nums.length === 1) return nums[0];
+
+  let left = 0;
+  let right = nums.length - 1;
+  if (nums[right] > nums[0]) {
+    return nums[0];
+  }
+
+  while (right >= left) {
+    const mid = Math.floor((left + right) / 2);
+
+    // The Change Point Here !!!
+    if (nums[mid] > nums[mid + 1]) {
+      return nums[mid + 1];
+    }
+    // Mid is the Change Point
+    if (nums[mid - 1] > nums[mid]) {
+      return nums[mid];
+    }
+
+    if (nums[mid] > nums[0]) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  return -1;
+};
+
+console.log(findMin([3, 4, 5, 1, 2]));
+console.log(findMin([11, 13, 15, 17]));
