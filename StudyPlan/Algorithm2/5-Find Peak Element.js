6d8df8effb08826ac5b10e5a2a8f6a6d98c74f4c
eslamelkholy <eslamelkholy5325762@gmail.com>
2022-05-07 20:17:20 +0200

    Find Peak Element Using Binar ySearch

---
+++ StudyPlan/Algorithm2/5-Find Peak Element.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findPeakElement = function (nums) {
+  let left = 0;
+  let right = nums.length - 1;
+  while (left < right) {
+    const mid = Math.floor((left + right) / 2);
+
+    if (nums[mid] > nums[mid + 1]) {
+      right = mid;
+    } else {
+      left = mid + 1;
+    }
+  }
+
+  return left;
+};
+
+console.log(findPeakElement([1, 2, 3, 1]));
+console.log(findPeakElement([1, 2, 1, 3, 5, 6, 4]));
