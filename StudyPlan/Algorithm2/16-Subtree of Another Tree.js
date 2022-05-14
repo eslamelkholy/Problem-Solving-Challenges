40f4b62f16ae1ede1a1b63a758bd185019991611
eslamelkholy <eslamelkholy5325762@gmail.com>
2022-05-14 18:23:56 +0200

    Subtree of Another Tree Using Recursive Solution

---
+++ StudyPlan/Algorithm2/16-Subtree of Another Tree.js
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} subRoot
+ * @return {boolean}
+ */
+var isSubtree = function (root, subRoot) {
+  let subTreeIdentical = false;
+  const dfs = (root) => {
+    if (!root) return null;
+
+    if (root.val === subRoot.val && isIdentical(subRoot.left, root.left) && isIdentical(subRoot.right, root.right)) {
+      subTreeIdentical = true;
+    }
+
+    dfs(root.left);
+    dfs(root.right);
+  };
+
+  const isIdentical = (sub, tree) => {
+    if (!sub && !tree) return true;
+    if (tree && !sub) return false;
+    if (sub && !tree) return false;
+
+    if (sub.val !== tree.val) return false;
+
+    return isIdentical(sub.left, tree.left) && isIdentical(sub.right, tree.right);
+  };
+  dfs(root);
+
+  return subTreeIdentical;
+};
