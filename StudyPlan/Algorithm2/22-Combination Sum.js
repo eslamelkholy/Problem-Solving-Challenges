8990c0cc2fd75e69436b2e4605259a3f6ae6ddb6
eslamelkholy <eslamelkholy5325762@gmail.com>
2022-05-15 23:45:32 +0200

    Combination Sum two Solution Backtracking

--- StudyPlan/Algorithm2/22-Combination Sum.js
+++ StudyPlan/Algorithm2/22-Combination Sum.js
@@ -5,17 +5,14 @@
  */
 var combinationSum = function (candidates, target) {
   const combinations = [];
-  /**
-   * @param {[]} combination
-   * @param {number} sum
-   */
+
   const exploreCombination = (combination, sum, index) => {
     if (sum > target) return;
     if (sum === target) combinations.push(combination);
 
     for (let i = index; i < candidates.length; i++) {
       const num = candidates[i];
-      if (num + sum > target) continue;
+      if (num + sum > target) break;
 
       exploreCombination([...combination, num], sum + num, i);
     }
@@ -25,22 +22,18 @@ var combinationSum = function (candidates, target) {
   return combinations;
 };
 
-var combinationSum1 = function (candidates, target) {
+var combinationSum2 = function (candidates, target) {
   const result = [];
 
-  const backtrack = (path = [], remain = target, start = 0) => {
-    // base case
-    if (remain < 0) {
-      return;
-    } else if (remain === 0) {
-      return result.push([...path]);
-    }
+  const backtrack = (path = [], sum = 0, start = 0) => {
+    if (sum > target) return;
+    if (sum === target) return result.push(path);
 
     for (let i = start; i < candidates.length; i++) {
       path.push(candidates[i]);
-      backtrack(path, remain - candidates[i], i);
 
-      // backtrack last added
+      backtrack([...path], sum + candidates[i], i);
+
       path.pop();
     }
   };
