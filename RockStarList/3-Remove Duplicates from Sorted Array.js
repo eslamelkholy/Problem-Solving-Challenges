f88df73f8d65ff086af041cfa2be998d7e19f735
eslamelkholy <eslamelkholy5325762@gmail.com>
2022-05-19 11:42:28 +0200

    Remove Duplicates from Sorted Array

---
+++ RockStarList/3-Remove Duplicates from Sorted Array.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function (nums) {
+  let i = 0;
+  for (let j = 0; j < nums.length; j++) {
+    if (nums[j] !== nums[i]) {
+      nums[++i] = nums[j];
+    }
+  }
+
+  return ++i;
+};
+
+console.log(removeDuplicates([1, 1, 2]));
+console.log(removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]));
