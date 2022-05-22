99012d73d5a34533b1727c6555c96cf80365ce41
eslamelkholy <eslamelkholy5325762@gmail.com>
2022-05-22 18:42:28 +0200

    Palindromic Substring Using DP

--- leetcodeDailyChallenge/2022/May/22-Palindromic Substrings.js
+++ leetcodeDailyChallenge/2022/May/22-Palindromic Substrings.js
@@ -2,18 +2,18 @@
  * @param {string} s
  * @return {number}
  */
-var countSubstrings = function(s) {
+var countSubstrings0 = function (s) {
   let palindromCounter = 0;
   for (let i = 0; i < s.length; i++) {
     let str = s[i];
     palindromCounter++;
-    for (let j = i+1; j < s.length; j++) {
+    for (let j = i + 1; j < s.length; j++) {
       str += s[j];
-      if(isPalindrome(str)) {
+      if (isPalindrome(str)) {
         palindromCounter++;
       }
-    }    
-  }    
+    }
+  }
   return palindromCounter;
 };
 const isPalindrome = (item) => {
@@ -22,4 +22,29 @@ const isPalindrome = (item) => {
     if (item[i] !== item[N - 1 - i]) return false;
   }
   return true;
+};
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const countSubstrings = (s) => {
+  const dp = new Array(s.length).fill(0).map(() => new Array(s.length).fill(0));
+  let count = 0;
+
+  for (let i = s.length - 1; i >= 0; i--) {
+    dp[i][i] = 1;
+    count++;
+
+    for (let j = i + 1; j < s.length; j++) {
+      if (s[i] === s[j]) {
+        if (i + 1 > j - 1 || dp[i + 1][j - 1] === 1) {
+          dp[i][j] = 1;
+          count++;
+        }
+      }
+    }
+  }
+
+  return count;
 };
