195a41ca59a61b06cf6ee1f5ee586240cb7c1bbc
eslamelkholy <eslamelkholy5325762@gmail.com>
2022-05-23 00:45:55 +0200

    LRU Cache Stable Version

--- RockStarList/5-LRU Cache.js
+++ RockStarList/5-LRU Cache.js
@@ -19,7 +19,7 @@ LRUCache.prototype.get = function (key) {
     return -1;
   }
   this.updateLatestPriority(key);
-  return this.cache;
+  return this.cache[key].value;
 };
 
 /**
@@ -29,9 +29,8 @@ LRUCache.prototype.get = function (key) {
  */
 LRUCache.prototype.put = function (key, value) {
   if (this.cache[key]) {
-    if (this.cache[this.latest]) this.cache[this.latest].next = key;
-    this.cache[key] = { prev: this.latest, next: null, value };
-    this.latest = key;
+    this.updateLatestPriority(key);
+    this.cache[key].value = value;
     return this.cache;
   }
 
@@ -44,6 +43,8 @@ LRUCache.prototype.put = function (key, value) {
 
 // Tested and Works Fine
 LRUCache.prototype.addNode = function (key, value) {
+  this.cache.capacity++;
+
   if (this.cache.latest === null) {
     this.cache[key] = { value, next: null, prev: null };
     this.cache['latest'] = key;
@@ -67,6 +68,11 @@ LRUCache.prototype.updateLatestPriority = function (key) {
   const currentNode = this.cache[key];
   const prevOfCurrentNode = currentNode.prev;
 
+  // Update Head if the Key is Equal to Head
+  if (key === this.cache.head) {
+    if (currentNode.next) this.cache.head = currentNode.next;
+  }
+
   if (currentNode.next === null) return; // 2- This is Already the last Node
 
   this.cache[latestTemp] = { ...this.cache[latestTemp], next: key }; // 2- Update Last Node to be Before this Node
@@ -76,15 +82,30 @@ LRUCache.prototype.updateLatestPriority = function (key) {
   }
   // If Previous Exists
   if (prevOfCurrentNode) {
-    this.cache[prevOfCurrentNode].next = currentNode.next; // Update Previous Next to Point to Latest
-    this.cache[currentNode.next].prev = prevOfCurrentNode;
+    if (this.cache[prevOfCurrentNode]) this.cache[prevOfCurrentNode].next = currentNode.next; // Update Previous Next to Point to Latest
+    if (this.cache[currentNode.next]) this.cache[currentNode.next].prev = prevOfCurrentNode;
   }
 
   this.cache[key] = { ...currentNode, prev: this.cache.latest, next: null }; // 3- Update Node to be the last One
   this.cache.latest = key;
 };
 
-LRUCache.prototype.updatedOverflow = function () {};
+LRUCache.prototype.updatedOverflow = function () {
+  const currentHeadNode = this.cache[this.cache.head];
+  delete this.cache[this.cache.head];
+  this.cache.capacity--;
+
+  if (!this.cache[this.cache.latest]) {
+    this.cache.head = null;
+    this.cache.latest = null;
+    return;
+  }
+
+  const nextHead = currentHeadNode.next;
+  this.cache[nextHead].prev = null; // Update next Head Prev Value
+
+  this.cache.head = nextHead;
+};
 
 console.log('NEWWWWWWWWWWWWWWWWWWWWWWWWWWWWW5');
 
@@ -96,63 +117,63 @@ console.log(lruCache5.put(4, 4)); //
 console.log(lruCache5.get(4)); // 4
 console.log(lruCache5.get(3)); // 3
 console.log(lruCache5.get(2)); // 2
-// console.log(lruCache5.get(1)); // -1
+console.log(lruCache5.get(1)); // -1
 console.log(lruCache5.put(5, 5)); //
-// console.log(lruCache5.get(1)); // -1
+console.log(lruCache5.get(1)); // -1
 console.log(lruCache5.get(2)); // 2
 console.log(lruCache5.get(3)); // 3
 console.log(lruCache5.get(4)); // -1
 console.log(lruCache5.get(5)); // 5
 
-// console.log('NEWWWWWWWWWWWWWWWWWWWWWWWWWWWWW1');
+console.log('NEWWWWWWWWWWWWWWWWWWWWWWWWWWWWW1');
 
-// const lruCache = new LRUCache(2);
-// console.log(lruCache.put(1, 1)); // cache is {1=1}
-// console.log(lruCache.put(2, 2)); // cache is {1=1, 2=2}
-// console.log(lruCache.get(1)); // return 1
-// console.log(lruCache.put(3, 3)); // LRU key was 2, evicts key 2, cache is {1=1, 3=3}
-// console.log(lruCache.get(2)); // returns -1 (not found)
-// console.log(lruCache.put(4, 4)); // LRU key was 1, evicts key 1, cache is {4=4, 3=3}
-// console.log(lruCache.get(1)); // return -1 (not found)
-// console.log(lruCache.get(3)); // return 3
-// console.log(lruCache.get(4)); // return 4
+const lruCache = new LRUCache(2);
+console.log(lruCache.put(1, 1)); // cache is {1=1}
+console.log(lruCache.put(2, 2)); // cache is {1=1, 2=2}
+console.log(lruCache.get(1)); // return 1
+console.log(lruCache.put(3, 3)); // LRU key was 2, evicts key 2, cache is {1=1, 3=3}
+console.log(lruCache.get(2)); // returns -1 (not found)
+console.log(lruCache.put(4, 4)); // LRU key was 1, evicts key 1, cache is {4=4, 3=3}
+console.log(lruCache.get(1)); // return -1 (not found)
+console.log(lruCache.get(3)); // return 3
+console.log(lruCache.get(4)); // return 4
 
-// console.log('NEWWWWWWWWWWWWWWWWWWWWWWWWWWWWW2');
-// /**
-//  * Your LRUCache object will be instantiated and called as such:
-//  * var obj = new LRUCache(capacity)
-//  * var param_1 = obj.get(key)
-//  * obj.put(key,value)
-//  */
+console.log('NEWWWWWWWWWWWWWWWWWWWWWWWWWWWWW3');
 
-// const lruCache2 = new LRUCache(1);
-// console.log(lruCache2.put(2, 1)); // cache is {2=1}
-// console.log(lruCache2.get(2)); // return 1
-// console.log(lruCache2.put(3, 2)); // LRU key was 2, evicts key 2, cache is { 3=2}
-// console.log(lruCache2.get(2)); // -1
-// console.log(lruCache2.get(3)); // 2
+const lruCache3 = new LRUCache(2);
+console.log(lruCache3.get(2)); // return -1
+console.log(lruCache3.put(2, 6)); // cache is {2=6}
+console.log(lruCache3.get(1)); // return -1
+console.log(lruCache3.put(1, 5)); // cache is {2=6, 1=5}
+console.log(lruCache3.put(1, 2)); // cache is {1=2, 1=5}
+console.log(lruCache3.get(1)); // 2
+console.log(lruCache3.get(2)); // -1
 
-// console.log('NEWWWWWWWWWWWWWWWWWWWWWWWWWWWWW3');
+['LRUCache', 'put', 'put', 'put', 'put', 'get', 'get'][([2], [2, 1], [1, 1], [2, 3], [4, 1], [1], [2])];
 
-// const lruCache3 = new LRUCache(2);
-// console.log(lruCache3.get(2)); // return -1
-// console.log(lruCache3.put(2, 6)); // cache is {2=6}
-// console.log(lruCache3.get(1)); // return -1
-// console.log(lruCache3.put(1, 5)); // cache is {2=6, 1=5}
-// console.log(lruCache3.put(1, 2)); // cache is {1=2, 1=5}
-// console.log(lruCache3.get(1)); // 2
-// console.log(lruCache3.get(2)); // -1
+console.log('NEWWWWWWWWWWWWWWWWWWWWWWWWWWWWW4');
 
-// ['LRUCache', 'put', 'put', 'put', 'put', 'get', 'get'][([2], [2, 1], [1, 1], [2, 3], [4, 1], [1], [2])];
+const lruCache4 = new LRUCache(2);
+console.log(lruCache4.get(2)); // return -1
+console.log(lruCache4.put(2, 1)); // cache is {2=1}
+console.log(lruCache4.put(1, 1)); // cache is {2=6}
+console.log(lruCache4.put(2, 3)); // cache is {2=6}
+console.log(lruCache4.put(4, 1)); // cache is {2=6}
+console.log(lruCache4.get(1)); // -1
+console.log(lruCache4.get(2)); // 3
+console.log(lruCache4.get(4)); // 1
 
-// console.log('NEWWWWWWWWWWWWWWWWWWWWWWWWWWWWW4');
+console.log('NEWWWWWWWWWWWWWWWWWWWWWWWWWWWWW2');
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * var obj = new LRUCache(capacity)
+ * var param_1 = obj.get(key)
+ * obj.put(key,value)
+ */
 
-// const lruCache4 = new LRUCache(2);
-// console.log(lruCache4.get(2)); // return -1
-// console.log(lruCache4.put(2, 1)); // cache is {2=1}
-// console.log(lruCache4.put(1, 1)); // cache is {2=6}
-// console.log(lruCache4.put(2, 3)); // cache is {2=6}
-// console.log(lruCache4.put(4, 1)); // cache is {2=6}
-// console.log(lruCache4.get(1)); // -1
-// console.log(lruCache4.get(2)); // 3
-// console.log(lruCache4.get(4)); // 1
+const lruCache2 = new LRUCache(1);
+console.log(lruCache2.put(2, 1)); // cache is {2=1}
+console.log(lruCache2.get(2)); // return 1
+console.log(lruCache2.put(3, 2)); // LRU key was 2, evicts key 2, cache is { 3=2}
+console.log(lruCache2.get(2)); // -1
+console.log(lruCache2.get(3)); // 2
