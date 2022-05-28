65d66d3903785619d16e3f45518d57e7352c7662
eslamelkholy <55209776+eslamelkholy@users.noreply.github.com>
2022-05-28 15:06:59 +0200

    Missing Numbers Without Extra Space Solution

--- leetcodeDailyChallenge/2022/May/28-Missing Number.js
+++ leetcodeDailyChallenge/2022/May/28-Missing Number.js
@@ -13,5 +13,11 @@ var missingNumber = function(nums) {
     return -1;
 };
 
-
-
+var missingNumber = function(nums) {
+    nums.sort((a,b) => a - b)
+    
+    for(let i = 0; i <= nums.length; i++) {
+        if(nums[i] !== i) return i;
+    }
+    return -1;
+};
