e0f7e9dfd7caad9c5cd86a98fbe187c79b4bfb06
eslamelkholy <eslamelkholy5325762@gmail.com>
2022-06-01 11:37:15 +0200

    Running Sum Array Without Extra Space

--- leetcodeDailyChallenge/2022/June/1-Running Sum of 1d Array.js
+++ leetcodeDailyChallenge/2022/June/1-Running Sum of 1d Array.js
@@ -2,13 +2,21 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var runningSum = function (nums) {
+var runningSum0 = function (nums) {
   const result = [nums[0]];
   for (let i = 1; i < nums.length; i++) {
     result.push(result[result.length - 1] + nums[i]);
   }
 
   return result;
+};
+
+var runningSum = function (nums) {
+  for (let i = 1; i < nums.length; i++) {
+    nums[i] += nums[i - 1];
+  }
+
+  return nums;
 };
 
 console.log(runningSum([1, 2, 3, 4]));
