b7b3a147b94bb0794b9f569f0909c58b7a50d125
eslamelkholy <eslamelkholy5325762@gmail.com>
2022-06-03 14:58:12 +0200

    Sum Range Query 2D Brute Force Solution

---
+++ leetcodeDailyChallenge/2022/June/3-Range Sum Query 2D - Immutable.js
@@ -0,0 +1,43 @@
+/**
+ * @param {number[][]} matrix
+ */
+class NumMatrix {
+  constructor(matrix) {
+    this.matrix = matrix;
+  }
+  /**
+   * @param {number} row1
+   * @param {number} col1
+   * @param {number} row2
+   * @param {number} col2
+   * @return {number}
+   */
+  sumRegion(row1, col1, row2, col2) {
+    let sum = 0;
+    for (let i = row1; i <= row2; i++) {
+      for (let j = col1; j <= col2; j++) {
+        sum += this.matrix[i][j];
+      }
+    }
+
+    return sum;
+  }
+}
+
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * var obj = new NumMatrix(matrix)
+ * var param_1 = obj.sumRegion(row1,col1,row2,col2)
+ */
+
+const numMatrix = new NumMatrix([
+  [3, 0, 1, 4, 2],
+  [5, 6, 3, 2, 1],
+  [1, 2, 0, 1, 5],
+  [4, 1, 0, 1, 7],
+  [1, 0, 3, 0, 5],
+]);
+
+console.log(numMatrix.sumRegion(2, 1, 4, 3)); // return 8 (i.e sum of the red rectangle)
+console.log(numMatrix.sumRegion(1, 1, 2, 2)); // return 11 (i.e sum of the green rectangle)
+console.log(numMatrix.sumRegion(1, 2, 2, 4)); // return 12 (i.e sum of the blue rectangle)
