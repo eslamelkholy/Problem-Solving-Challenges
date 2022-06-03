cbecd0c32dc4208085fcf9e66111fb36c2797d39
eslamelkholy <eslamelkholy5325762@gmail.com>
2022-06-03 21:04:01 +0200

    Populating Next Right Pointer

---
+++ Learning/DFS-BFS/2-Populating Next Right Pointers in Each Node II.js
@@ -0,0 +1,36 @@
+/**
+ * // Definition for a Node.
+ * function Node(val, left, right, next) {
+ *    this.val = val === undefined ? null : val;
+ *    this.left = left === undefined ? null : left;
+ *    this.right = right === undefined ? null : right;
+ *    this.next = next === undefined ? null : next;
+ * };
+ */
+
+/**
+ * @param {Node} root
+ * @return {Node}
+ */
+var connect = function (root) {
+  if (!root) return root;
+  const queue = [root];
+
+  while (queue.length > 0) {
+    const N = queue.length;
+
+    for (let i = 0; i < N; i++) {
+      const currentNode = queue.shift();
+
+      if (i === N - 1) {
+        currentNode.next = null;
+      } else {
+        currentNode.next = queue[0];
+      }
+
+      if (currentNode.left) queue.push(currentNode.left);
+      if (currentNode.right) queue.push(currentNode.right);
+    }
+  }
+  return root;
+};
