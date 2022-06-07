45f1674ca7aae1814bb4635c534f556b402e5a6a
eslamelkholy <eslamelkholy5325762@gmail.com>
2022-06-07 20:00:56 +0200

    Find all Anagrams using Sliding Window

--- RockStarList/8-Find All Anagrams in a String.js
+++ RockStarList/8-Find All Anagrams in a String.js
@@ -3,7 +3,46 @@
  * @param {string} p
  * @return {number[]}
  */
-var findAnagrams = function (s, p) {};
+var findAnagrams = function (s, p) {
+  const charMap = {};
+  const currentCharMap = {};
+  for (const char of p) {
+    charMap[char] ? charMap[char]++ : (charMap[char] = 1);
+  }
+  let result = [];
+  let left = 0;
+  let right = 0;
+
+  while (right < s.length) {
+    const char = s[right];
+    currentCharMap[char] ? currentCharMap[char]++ : (currentCharMap[char] = 1);
+
+    if (isAnagram(charMap, currentCharMap)) {
+      result.push(left);
+    }
+
+    const currentWindowLength = right - left + 1;
+    if (currentWindowLength === p.length) {
+      if (currentCharMap[s[left]]) currentCharMap[s[left]]--;
+      left++;
+    }
+
+    right++;
+  }
+  return result;
+};
+
+/**
+ * @param {Map} map1
+ * @param {Map} map2
+ * @returns {boolean}
+ */
+const isAnagram = (map1, map2) => {
+  for (const key in map1) {
+    if (map2[key] === undefined || map2[key] !== map1[key]) return false;
+  }
+  return true;
+};
 
 console.log(findAnagrams('cbaebabacd', 'abc'));
 console.log(findAnagrams('abab', 'ab'));
