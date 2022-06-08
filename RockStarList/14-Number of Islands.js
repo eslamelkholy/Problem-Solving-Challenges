5aa6f6f63e0e2b091116f27772aaee1897b2cf12
eslamelkholy <eslamelkholy5325762@gmail.com>
2022-06-08 19:51:42 +0200

    Number of Islands More Optimization

--- RockStarList/14-Number of Islands.js
+++ RockStarList/14-Number of Islands.js
@@ -2,29 +2,58 @@
  * @param {character[][]} grid
  * @return {number}
  */
-var numIslands = function (grid) {
+var numIslands0 = function (grid) {
   let islands = 0;
 
   for (let i = 0; i < grid.length; i++) {
     for (let j = 0; j < grid[i].length; j++) {
-      islands += traverseIslands(grid, i, j);
+      islands += traverseIslands0(grid, i, j);
     }
   }
   return islands;
 };
 
-const traverseIslands = (grid, x, y) => {
+const traverseIslands0 = (grid, x, y) => {
   if (grid[x] === undefined || grid[x][y] === undefined || grid[x][y] === '0') {
     return 0;
   }
   grid[x][y] = '0';
 
-  traverseIslands(grid, x + 1, y);
-  traverseIslands(grid, x - 1, y);
-  traverseIslands(grid, x, y + 1);
-  traverseIslands(grid, x, y - 1);
+  traverseIslands0(grid, x + 1, y);
+  traverseIslands0(grid, x - 1, y);
+  traverseIslands0(grid, x, y + 1);
+  traverseIslands0(grid, x, y - 1);
 
   return 1;
+};
+
+/**
+ * @param {character[][]} grid
+ * @return {number}
+ */
+var numIslands = function (grid) {
+  let islands = 0;
+
+  const traverseIslands = (x, y) => {
+    if (grid[x] === undefined || grid[x][y] === undefined || grid[x][y] === '0') {
+      return 0;
+    }
+    grid[x][y] = '0';
+
+    traverseIslands(x + 1, y);
+    traverseIslands(x - 1, y);
+    traverseIslands(x, y + 1);
+    traverseIslands(x, y - 1);
+
+    return 1;
+  };
+
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
+      islands += traverseIslands(i, j);
+    }
+  }
+  return islands;
 };
 
 console.log(
