6c76be603cbbf8ef2ed1b8fbadb27f6b0c13d0ca
eslamelkholy <eslamelkholy5325762@gmail.com>
2022-06-08 13:45:56 +0200

    Remove Palindromic SubSequence

---
+++ leetcodeDailyChallenge/2022/June/8-Remove Palindromic Subsequences.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var removePalindromeSub = function (s) {
+  if (isPalindrom(s)) return 1;
+  for (let i = 0; i < s.length; i++) {
+    let subSeq = s[i];
+    for (let j = i + 1; j < s.length; j++) {
+      subSeq += s[j];
+      if (isPalindrom(subSeq)) {
+        return 2;
+      }
+    }
+  }
+  return 1;
+};
+
+const isPalindrom = (s) => {
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] !== s[s.length - i - 1]) return false;
+  }
+  return true;
+};
+
+console.log(removePalindromeSub('ababa'));
+console.log(removePalindromeSub('baabb'));
+console.log(removePalindromeSub('abb'));
