4a2ffae00666f4ad48efa534177d730d2886964b
eslamelkholy <eslamelkholy5325762@gmail.com>
2022-06-10 14:50:02 +0200

    Added The Commented Version of Sliding Window

--- leetcodeDailyChallenge/2022/June/10-Longest Substring Without Repeating Characters.js
+++ leetcodeDailyChallenge/2022/June/10-Longest Substring Without Repeating Characters.js
@@ -26,6 +26,32 @@ var lengthOfLongestSubstring = function (s) {
   return longestLength;
 };
 
+var lengthOfLongestSubstringC = function (s) {
+  let charSet = new Set();
+  let longestLength = 0;
+  let left = 0; // 1- Left Pointer
+  let right = 0; // 2- Right Pointer
+
+  while (right < s.length) {
+    const char = s[right]; // 3- Current Character we Have
+
+    while (charSet.has(char)) {
+      // 4- If the character already Exists in Current Window Loop Till we Remove Duplication
+      charSet.delete(s[left]);
+
+      left++;
+    }
+    charSet.add(char);
+
+    // 5- Compare Current Window Length and Set the Maximum Length
+    longestLength = Math.max(longestLength, charSet.size);
+
+    right++; // 6- Increase the Length of the Window of Each Iterate
+  }
+
+  return longestLength;
+};
+
 console.log(lengthOfLongestSubstring('abcabcbb'));
 console.log(lengthOfLongestSubstring('bbbbb'));
 console.log(lengthOfLongestSubstring('pwwkew'));
