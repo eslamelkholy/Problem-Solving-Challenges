9ba740eff148c01fbf0155b433c586ed18d46b2a
eslamelkholy <eslamelkholy5325762@gmail.com>
2022-06-18 14:00:19 +0200

    Subarray Sum More Cleaner Solution

--- Learning/HashTable/22-Subarray Sum Equals K.js
+++ Learning/HashTable/22-Subarray Sum Equals K.js
@@ -22,3 +22,23 @@ var subarraySum = function (nums, k) {
   }
   return occuranceCount;
 };
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var subarraySum = function (nums, k) {
+  const numsMap = { 0: 1 };
+  let sum = 0;
+  let occuranceCount = 0;
+
+  for (let num of nums) {
+    sum += num;
+    if (numsMap[sum - k] !== undefined) {
+      occuranceCount += numsMap[sum - k];
+    }
+    numsMap[sum] !== undefined ? numsMap[sum]++ : (numsMap[sum] = 1);
+  }
+  return occuranceCount;
+};
