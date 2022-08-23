bb60a3058b4e5b95035ef0701fa166e6ae49161b
eslamelkholy <55209776+eslamelkholy@users.noreply.github.com>
2022-08-23 17:29:52 +0200

    Palindrom Linked List With Reverse Half

--- leetcodeDailyChallenge/2022/Aug/23-Palindrome Linked List.js
+++ leetcodeDailyChallenge/2022/Aug/23-Palindrome Linked List.js
@@ -9,20 +9,68 @@
  * @param {ListNode} head
  * @return {boolean}
  */
-var isPalindrome = function(head) {
-    let counter = 0;
-    const result = [];
-    while(head) {
-        counter++;
-        result.push(head.val);
-        head = head.next;
+// var isPalindrome = function(head) {
+//     let counter = 0;
+//     const result = [];
+//     while(head) {
+//         counter++;
+//         result.push(head.val);
+//         head = head.next;
+//     }
+    
+//     for(let i = 0; i < result.length / 2;i++) {
+//         if(result[i] != result[result.length - i - 1]) {
+//             return false;
+//         }
+//     }
+//     return true;
+// };
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+ var isPalindrome = function(head) {
+    let slow = head;
+    let fast = head;
+    
+    while(fast && fast.next) {
+        fast = fast.next.next;
+        slow = slow.next;
+    }
+    if(fast != null) {
+        slow = slow.next;  // Slow = less than the half of faster for (Odd)
     }
     
-    for(let i = 0; i < result.length / 2;i++) {
-        if(result[i] != result[result.length - i - 1]) {
+    slow = reverse(slow);
+    fast = head;
+    
+    while (slow != null) {
+        if (fast.val != slow.val) {
             return false;
         }
+        fast = fast.next;
+        slow = slow.next;
     }
     return true;
 };
 
+const reverse = (head) => {
+    let prev;
+    while(head) {
+        const next = head.next;
+        head.next = prev;
+        
+        prev = head;
+        head = next;
+    }
+    
+    return prev;
+}
