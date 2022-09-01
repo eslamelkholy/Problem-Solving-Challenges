363e5b07f949d4563675b23f61a1b6b2f0f832db
eslamelkholy <55209776+eslamelkholy@users.noreply.github.com>
2022-09-01 13:01:40 +0200

    Day1 at Sept Count Good Nodes

---
+++ leetcodeDailyChallenge/2022/Sept/1-Count Good Nodes in Binary Tree.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+ var goodNodes = function(root) {
+    let goodNodesCounter = 0;
+    
+    const inorder = (root, prev) => {
+        if(!root) return;
+        
+        if(root.val >= prev) {
+            goodNodesCounter++;
+            prev = root.val;
+        }
+        
+        inorder(root.left, prev);
+        inorder(root.right, prev);
+    }
+    
+    inorder(root, root.val);
+    return goodNodesCounter;
+};
