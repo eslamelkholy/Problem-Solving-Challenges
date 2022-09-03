df8f9be6f05370872ee0a099295b956cccb40e02
eslamelkholy <55209776+eslamelkholy@users.noreply.github.com>
2022-09-03 14:24:24 +0200

    Max Sum of Rectangle Using Kadanes Algorithm

--- Learning/Kadanes/3-Max Sum of Rectangle No Larger Than K.js
+++ Learning/Kadanes/3-Max Sum of Rectangle No Larger Than K.js
@@ -1,84 +1,40 @@
-const kadaneRunning = (runningSum, k) => {
-    let maxSum = runningSum[0];
-    let currentSum = 0;
-  
-    for (let i = 0; i < runningSum.length; i++) {
-      const num = runningSum[i];
-      currentSum = currentSum + num;
-  
-      if (currentSum < num) {
-        currentSum = num;
-      }
-  
-      if (maxSum < currentSum) {
-        maxSum = currentSum;
-      }
-      if (maxSum === k) return maxSum;
-    }
-  
-    return maxSum;
-  };
-  
-  /**
-   * @param {number[][]} matrix
-   * @param {number} k
-   * @return {number}
-   */
-  var maxSumSubmatrix = function (matrix, k) {
-    let runningSum = new Array(matrix.length);
+/**
+ * @param {number[][]} matrix
+ * @param {number} k
+ * @return {number}
+ */
+ var maxSumSubmatrix = function (matrix, k) {
+    const cols = matrix[0].length;
+    const rows = matrix.length;
     let maxSumRect = Number.MIN_SAFE_INTEGER;
+    const runningSum = new Array(rows).fill(0);
   
-    for (let i = 0; i < matrix.length; i++) {
-      for (let j = 0; j < matrix[i].length; j++) {
-        runningSum.fill(0);
-  
-        // Right Pointer Loop
-        for (let rightPointer = j; rightPointer < matrix[0].length; rightPointer++) {
-          // Calculate Current Running Row Sum
-          for (let runIndex = 0; runIndex < runningSum.length; runIndex++) {
-            runningSum[runIndex] += matrix[runIndex][rightPointer];
-          }
-          const currentRectangleSum = kadaneRunning(runningSum, k);
-          if (currentRectangleSum > maxSumRect && currentRectangleSum <= k) {
-            maxSumRect = currentRectangleSum;
-          }
+    for (let leftPointer = 0; leftPointer < cols; leftPointer++) {
+      runningSum.fill(0); // Reset Running sum to Zero
+      for (let rightPointer = leftPointer; rightPointer < cols; rightPointer++) {
+        for (let k1 = 0; k1 < rows; k1++) {
+          runningSum[k1] += matrix[k1][rightPointer];
         }
+        // TODO: Use Kadanes
+        maxSumRect = kadaneRunning(runningSum, k, maxSumRect);
       }
     }
     return maxSumRect;
   };
   
-  // console.log(
-  //   maxSumSubmatrix(
-  //     [
-  //       [1, 0, 1],
-  //       [0, -2, 3],
-  //     ],
-  //     2
-  //   )
-  // );
-  
-  // console.log(maxSumSubmatrix([[2, 2, -1]], 3));
-  
-  // console.log(maxSumSubmatrix([[2, 2, -1]], 0));
-  console.log(
-    maxSumSubmatrix(
-      [
-        [5, -4, -3, 4],
-        [-3, -4, 4, 5],
-        [5, 1, 5, -4],
-      ],
-      8
-    )
-  );
-  // console.log(
-  //   maxSumSubmatrix(
-  //     [
-  //       [5, -4, -3, 4],
-  //       [-3, -4, 4, 5],
-  //       [5, 1, 5, -4],
-  //     ],
-  //     10
-  //   )
-  // );
+  const kadaneRunning = (runningSum, k, maxSumRect) => {
+    let currSum = 0;
+    const set = [0];
+    for (let k1 = 0; k1 < runningSum.length; k1++) {
+      currSum += runningSum[k1];
+      for (const item of set) {
+        const num = currSum - item;
+        if (num <= k) {
+          maxSumRect = Math.max(maxSumRect, num);
+        }
+      }
+      set.push(currSum);
+    }
+    return maxSumRect;
+  };
   
