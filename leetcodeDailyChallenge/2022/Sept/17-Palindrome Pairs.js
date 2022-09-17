e12915a47929905f8a6d0aad59048cfa51d31645
eslamelkholy <55209776+eslamelkholy@users.noreply.github.com>
2022-09-17 14:24:34 +0200

    Memory limit Exceed Solution WTF

--- leetcodeDailyChallenge/2022/Sept/17-Palindrome Pairs.js
+++ leetcodeDailyChallenge/2022/Sept/17-Palindrome Pairs.js
@@ -20,3 +20,35 @@ const isPalindrom = (word) => {
   }
   return true;
 };
+
+/**
+ * @param {string[]} words
+ * @return {number[][]}
+ */
+var palindromePairs = function (words) {
+  const result = [];
+  const memo = new Map();
+
+  for (let i = 0; i < words.length; i++) {
+    for (let j = 0; j < words.length; j++) {
+      if (i === j) continue;
+      const word = words[i] + words[j];
+
+      if (!memo.has(word)) {
+        memo.set(word, isPalindrom(word));
+      }
+
+      if (memo.get(word)) {
+        result.push([i, j]);
+      }
+    }
+  }
+  return result;
+};
+
+const isPalindrom = (word) => {
+  for (let i = 0; i < word.length; i++) {
+    if (word[i] !== word[word.length - i - 1]) return false;
+  }
+  return true;
+};
