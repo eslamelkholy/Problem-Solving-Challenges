f72503220cb7c23e5e58842063a905a668bc7dd0
eslamelkholy <55209776+eslamelkholy@users.noreply.github.com>
2022-10-02 00:21:50 +0200

    Group anagarmas Solution

--- Learning/HashTable/13-Group Anagrams.js
+++ Learning/HashTable/13-Group Anagrams.js
@@ -5,10 +5,25 @@
 var groupAnagrams = function (strs) {
   const anagramMap = {};
   for (const str of strs) {
-    const sortedStr = str.split().sort().join('');
+    const sortedStr = str.split().sort().join("");
     anagramMap[sortedStr] ? anagramMap[sortedStr].push(str) : (anagramMap[sortedStr] = [str]);
   }
   return Object.values(anagramMap);
 };
 
-console.log(groupAnagrams(['eat', 'tea', 'tan', 'ate', 'nat', 'bat']));
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function (strs) {
+  const groupMap = {};
+
+  for (const str of strs) {
+    const strKey = str.split("").sort().join("");
+
+    groupMap[strKey] ? groupMap[strKey].push(str) : (groupMap[strKey] = [str]);
+  }
+  return Object.values(groupMap);
+};
+
+console.log(groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]));
