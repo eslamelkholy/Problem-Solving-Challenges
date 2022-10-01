61df5913bdc7cdd54b5dba5b2f703be1c84a4bbf
eslamelkholy <55209776+eslamelkholy@users.noreply.github.com>
2022-10-01 19:53:27 +0200

    Minimum Index Sum of Two Lists

--- Learning/HashTable/9-Minimum Index Sum of Two Lists.js
+++ Learning/HashTable/9-Minimum Index Sum of Two Lists.js
@@ -26,23 +26,57 @@ var findRestaurant = function (list1, list2) {
   return [result];
 };
 
-console.log(findRestaurant(['Shogun', 'Tapioca Express', 'Burger King', 'KFC'], ['KFC', 'Shogun', 'Burger King']));
+console.log(findRestaurant(["Shogun", "Tapioca Express", "Burger King", "KFC"], ["KFC", "Shogun", "Burger King"]));
 console.log(
   findRestaurant(
-    ['Shogun', 'Tapioca Express', 'Burger King', 'KFC'],
-    ['Piatti', 'The Grill at Torrey Pines', 'Hungry Hunter Steakhouse', 'Shogun']
+    ["Shogun", "Tapioca Express", "Burger King", "KFC"],
+    ["Piatti", "The Grill at Torrey Pines", "Hungry Hunter Steakhouse", "Shogun"]
   )
 );
 console.log(
   findRestaurant(
-    ['Shogun', 'Tapioca Express', 'Burger King', 'KFC'],
-    ['KFC', 'Burger King', 'Tapioca Express', 'Shogun']
+    ["Shogun", "Tapioca Express", "Burger King", "KFC"],
+    ["KFC", "Burger King", "Tapioca Express", "Shogun"]
   )
 );
 
 console.log(
   findRestaurant(
-    ['Shogun', 'Piatti', 'Tapioca Express', 'Burger King', 'KFC'],
-    ['Piatti', 'The Grill at Torrey Pines', 'Hungry Hunter Steakhouse', 'Shogun']
+    ["Shogun", "Piatti", "Tapioca Express", "Burger King", "KFC"],
+    ["Piatti", "The Grill at Torrey Pines", "Hungry Hunter Steakhouse", "Shogun"]
   )
 );
+
+/**
+ * @param {string[]} list1
+ * @param {string[]} list2
+ * @return {string[]}
+ */
+var findRestaurant = function (list1, list2) {
+  const listMap1 = {};
+  const listMap2 = {};
+  let leastIndexSum = Number.MAX_SAFE_INTEGER;
+  const result = [];
+
+  for (let i = 0; i < list1.length; i++) {
+    listMap1[list1[i]] = i;
+  }
+  for (let i = 0; i < list2.length; i++) {
+    const word = list2[i];
+
+    if (listMap1[word] !== undefined && i + listMap1[word] < leastIndexSum) {
+      leastIndexSum = i + listMap1[word];
+    }
+    listMap2[word] = i;
+  }
+
+  for (const key in listMap1) {
+    if (listMap2[key] === undefined) continue;
+
+    if (listMap2[key] + listMap1[key] === leastIndexSum) {
+      result.push(key);
+    }
+  }
+
+  return result;
+};
