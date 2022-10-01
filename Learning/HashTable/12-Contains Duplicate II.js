2097b3d5a33255211fc7d51d92794da23ac6eb6d
eslamelkholy <55209776+eslamelkholy@users.noreply.github.com>
2022-10-01 23:38:45 +0200

    Contains Duplicate II

--- Learning/HashTable/12-Contains Duplicate II.js
+++ Learning/HashTable/12-Contains Duplicate II.js
@@ -12,6 +12,24 @@ var containsNearbyDuplicate = function (nums, k) {
   }
   return false;
 };
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsNearbyDuplicate = function (nums, k) {
+  const numMap = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    const num = nums[i];
+
+    if (numMap[num] !== undefined && Math.abs(i - numMap[num]) <= k) {
+      return true;
+    }
+    numMap[num] = i;
+  }
+  return false;
+};
 
 console.log(containsNearbyDuplicate([1, 2, 3, 1], 3));
 console.log(containsNearbyDuplicate([1, 0, 1, 1], 1));
