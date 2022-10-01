67d2f9ad585083a6834e6f980ed2aacae4de16d5
eslamelkholy <55209776+eslamelkholy@users.noreply.github.com>
2022-10-01 19:26:05 +0200

    Two Sum Solution O(N)

--- Learning/HashTable/7-Two Sum.js
+++ Learning/HashTable/7-Two Sum.js
@@ -26,6 +26,23 @@ var twoSum = function (nums, target) {
   }
 };
 
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+  const numMap = {};
+  for (let i = 0; i < nums.length; i++) {
+    const sumVal = target - nums[i];
+
+    if (numMap[sumVal] !== undefined) {
+      return [i, numMap[sumVal]];
+    }
+    numMap[nums[i]] = i;
+  }
+};
+
 console.log(twoSum([2, 7, 11, 15], 9));
 // console.log(twoSum([3, 3], 6));
 console.log(twoSum([3, 2, 4], 6));
