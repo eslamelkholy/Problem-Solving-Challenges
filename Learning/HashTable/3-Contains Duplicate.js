637936092d0655931b2089e7e1e871d72bc85144
eslamelkholy <55209776+eslamelkholy@users.noreply.github.com>
2022-10-01 14:56:56 +0200

    Contains Duplicate Problem

--- Learning/HashTable/3-Contains Duplicate.js
+++ Learning/HashTable/3-Contains Duplicate.js
@@ -3,10 +3,14 @@
  * @return {boolean}
  */
 var containsDuplicate = function (nums) {
-  const set = new Set();
+  const numSet = new Set();
   for (const num of nums) {
-    if (set.has(num)) return true;
-    set.add(num);
+    if (numSet.has(num)) return true;
+
+    numSet.add(num);
   }
+
   return false;
 };
+
+console.log(containsDuplicate([1, 2, 3, 1]));
