be9e64081058d35e04ea565168179e85d8973ff1
eslamelkholy <55209776+eslamelkholy@users.noreply.github.com>
2022-10-01 20:11:03 +0200

    First Unique Character in String O(N)

--- Learning/HashTable/10-First Unique Character in a String.js
+++ Learning/HashTable/10-First Unique Character in a String.js
@@ -4,13 +4,31 @@
  */
 var firstUniqChar = function (s) {
   const strMap = {};
-  for (let i = 0; i < s.length; i++) strMap[s[i]] ? strMap[s[i]]['count']++ : (strMap[s[i]] = { count: 1, index: i });
+  for (let i = 0; i < s.length; i++)
+    strMap[s[i]] ? strMap[s[i]]["count"]++ : (strMap[s[i]] = { count: 1, index: i });
 
-  for (const char in strMap) if (strMap[char]['count'] === 1) return strMap[char]['index'];
+  for (const char in strMap) if (strMap[char]["count"] === 1) return strMap[char]["index"];
 
   return -1;
 };
 
-console.log(firstUniqChar('leetcode'));
-console.log(firstUniqChar('loveleetcode'));
-console.log(firstUniqChar('aabb'));
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var firstUniqChar = function (s) {
+  const freqMap = {};
+  for (const char of s) {
+    freqMap[char] ? freqMap[char]++ : (freqMap[char] = 1);
+  }
+
+  for (let i = 0; i < s.length; i++) {
+    if (freqMap[s[i]] === 1) return i;
+  }
+
+  return -1;
+};
+
+console.log(firstUniqChar("leetcode"));
+console.log(firstUniqChar("loveleetcode"));
+console.log(firstUniqChar("aabb"));
