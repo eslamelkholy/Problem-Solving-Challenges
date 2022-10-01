25de70d5b7beabb7ce0ceaf4973a32a373d6fd8a
eslamelkholy <55209776+eslamelkholy@users.noreply.github.com>
2022-10-01 19:41:12 +0200

    IsoMorphic String Solution O(N)

--- Learning/HashTable/8-Isomorphic Strings.js
+++ Learning/HashTable/8-Isomorphic Strings.js
@@ -24,4 +24,28 @@ var isIsomorphic = function (s, t) {
   return true;
 };
 
-console.log(isIsomorphic('egg', 'add'));
+console.log(isIsomorphic("egg", "add"));
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isIsomorphic = function (s, t) {
+  const sMap = {};
+  const tMap = {};
+
+  for (let i = 0; i < s.length; i++) {
+    const sChar = s[i];
+    const tChar = t[i];
+    if (sMap[sChar] === undefined && tMap[tChar] === undefined) {
+      sMap[sChar] = tChar;
+      tMap[tChar] = sChar;
+    }
+
+    if (sMap[sChar] !== tChar || tMap[tChar] !== sChar) {
+      return false;
+    }
+  }
+
+  return true;
+};
