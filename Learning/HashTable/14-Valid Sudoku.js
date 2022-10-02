6d706c559f6039e748596baba57937b10f9a64e1
eslamelkholy <55209776+eslamelkholy@users.noreply.github.com>
2022-10-02 22:17:34 +0200

    Valid Sudoku Solution using Sets

--- Learning/HashTable/14-Valid Sudoku.js
+++ Learning/HashTable/14-Valid Sudoku.js
@@ -15,7 +15,7 @@ function isValidSudoku(board) {
     for (let j = 0; j < board.length; j++) {
       let cell = board[i][j];
 
-      if (cell !== '.') {
+      if (cell !== ".") {
         if (rows[i].includes(cell)) {
           return false;
         } else rows[i].push(cell);
@@ -35,16 +35,63 @@ function isValidSudoku(board) {
 
   return true;
 }
+
+/**
+ * @param {character[][]} board
+ * @return {boolean}
+    1-Each row must contain the digits 1-9 without repetition.
+    2-Each column must contain the digits 1-9 without repetition.
+    3-Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+ */
+function isValidSudoku(board) {
+  const rows = [];
+  const cols = [];
+  const boxes = [];
+  const N = 9;
+
+  for (let i = 0; i < 9; i++) {
+    rows.push(new Set());
+    cols.push(new Set());
+    boxes.push(new Set());
+  }
+
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[i].length; j++) {
+      const cell = board[i][j];
+      if (cell === ".") continue;
+
+      if (rows[i].has(cell)) {
+        return false;
+      } else {
+        rows[i].add(cell);
+      }
+
+      if (cols[j].has(cell)) {
+        return false;
+      } else {
+        cols[j].add(cell);
+      }
+
+      let boxIndex = Math.floor(i / 3) * 3 + Math.floor(j / 3);
+      if (boxes[boxIndex].has(cell)) {
+        return false;
+      } else {
+        boxes[boxIndex].add(cell);
+      }
+    }
+  }
+  return true;
+}
 console.log(
   isValidSudoku([
-    ['5', '3', '.', '.', '7', '.', '.', '.', '.'],
-    ['6', '.', '.', '1', '9', '5', '.', '.', '.'],
-    ['.', '9', '8', '.', '.', '.', '.', '6', '.'],
-    ['8', '.', '.', '.', '6', '.', '.', '.', '3'],
-    ['4', '.', '.', '8', '.', '3', '.', '.', '1'],
-    ['7', '.', '.', '.', '2', '.', '.', '.', '6'],
-    ['.', '6', '.', '.', '.', '.', '2', '8', '.'],
-    ['.', '.', '.', '4', '1', '9', '.', '.', '5'],
-    ['.', '.', '.', '.', '8', '.', '.', '7', '9'],
+    ["5", "3", ".", ".", "7", ".", ".", ".", "."],
+    ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+    [".", "9", "8", ".", ".", ".", ".", "6", "."],
+    ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+    ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+    ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+    [".", "6", ".", ".", ".", ".", "2", "8", "."],
+    [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+    [".", ".", ".", ".", "8", ".", ".", "7", "9"],
   ])
 );
