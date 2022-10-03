fb27f0417475781538b0b9065f341dc11e4dbd0d
eslamelkholy <55209776+eslamelkholy@users.noreply.github.com>
2022-10-03 19:26:06 +0200

    Jewels and Stones Solution

--- Learning/HashTable/16-Jewels and Stones.js
+++ Learning/HashTable/16-Jewels and Stones.js
@@ -10,3 +10,19 @@ var numJewelsInStones = function (jewels, stones) {
   for (const stone of stones) if (jewelsSet.has(stone)) count++;
   return count;
 };
+
+/**
+ * @param {string} jewels
+ * @param {string} stones
+ * @return {number}
+ */
+var numJewelsInStones = function (jewels, stones) {
+  const jewelsSet = new Set(jewels.split(""));
+  let numOfJewels = 0;
+
+  for (const stone of stones) {
+    if (jewelsSet.has(stone)) numOfJewels++;
+  }
+
+  return numOfJewels;
+};
