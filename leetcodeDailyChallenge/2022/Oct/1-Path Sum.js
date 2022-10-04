0aefbb979f8b4ac2115375774ea3c0a88b2e96d8
eslamelkholy <55209776+eslamelkholy@users.noreply.github.com>
2022-10-04 19:38:38 +0200

    Path Sum Solution

--- leetcodeDailyChallenge/2022/Oct/1-Path Sum.js
+++ leetcodeDailyChallenge/2022/Oct/1-Path Sum.js
@@ -29,3 +29,31 @@ var hasPathSum = function (root, targetSum) {
   dfs(root, 0);
   return result;
 };
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {boolean}
+ */
+var hasPathSum = function (root, targetSum) {
+  const dfs = (root, previousVal) => {
+    if (!root) return false;
+
+    const currentVal = root.val + previousVal;
+    if (currentVal === targetSum && !root.left && !root.right) {
+      return true;
+    }
+
+    return dfs(root.left, currentVal) || dfs(root.right, currentVal);
+  };
+
+  return dfs(root, 0);
+};
