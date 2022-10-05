d8eee14a0aaf27bfd346c9a40632cf1a8eca2b63
eslamelkholy <55209776+eslamelkholy@users.noreply.github.com>
2022-10-05 20:47:18 +0200

    Four Some Solution Using Hashtable

--- Learning/HashTable/18-4Sum II.js
+++ Learning/HashTable/18-4Sum II.js
@@ -26,3 +26,32 @@ var fourSumCount = function (A, B, C, D) {
 
   return res;
 };
+
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @param {number[]} nums3
+ * @param {number[]} nums4
+ * @return {number}
+ */
+var fourSumCount = function (nums1, nums2, nums3, nums4) {
+  let numOfFourSum = 0;
+  const map = {};
+
+  for (const num1 of nums1) {
+    for (const num2 of nums2) {
+      const sum = num1 + num2;
+      map[sum] !== undefined ? map[sum]++ : (map[sum] = 1);
+    }
+  }
+
+  for (const num3 of nums3) {
+    for (const num4 of nums4) {
+      const sum = num3 + num4;
+      if (map[-sum] !== undefined) {
+        numOfFourSum += map[-sum];
+      }
+    }
+  }
+  return numOfFourSum;
+};
