f9c3d625b5fdd8c56e3e15d7a69004a40e0e6b9e
eslamelkholy <55209776+eslamelkholy@users.noreply.github.com>
2022-10-06 20:22:33 +0200

    Time Based Key - Value Solution using Hash tables linear search

--- leetcodeDailyChallenge/2022/Oct/6-Time Based Key-Value.js
+++ leetcodeDailyChallenge/2022/Oct/6-Time Based Key-Value.js
@@ -42,3 +42,44 @@ TimeMap.prototype.get = function (key, timestamp) {
  * obj.set(key,value,timestamp)
  * var param_2 = obj.get(key,timestamp)
  */
+
+var TimeMap = function () {
+  this.map = {};
+};
+
+/**
+ * @param {string} key
+ * @param {string} value
+ * @param {number} timestamp
+ * @return {void}
+ */
+TimeMap.prototype.set = function (key, value, timestamp) {
+  if (this.map[key] === undefined) {
+    this.map[key] = new Map();
+  }
+  this.map[key].set(timestamp, value);
+};
+
+/**
+ * @param {string} key
+ * @param {number} timestamp
+ * @return {string}
+ */
+TimeMap.prototype.get = function (key, timestamp) {
+  if (this.map[key] === undefined) return "";
+
+  if (this.map[key].get(timestamp) !== undefined) return this.map[key].get(timestamp);
+
+  for (let i = timestamp; i >= 1; i--) {
+    if (this.map[key].get(i)) return this.map[key].get(i);
+  }
+
+  return "";
+};
+
+/**
+ * Your TimeMap object will be instantiated and called as such:
+ * var obj = new TimeMap()
+ * obj.set(key,value,timestamp)
+ * var param_2 = obj.get(key,timestamp)
+ */
