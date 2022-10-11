1cc6dc9278578a0d2296d7083216909662461a18
eslamelkholy <55209776+eslamelkholy@users.noreply.github.com>
2022-10-11 23:37:17 +0200

    Implement StrStr() Function

---
+++ Learning/Array And Strings/8-Implement strStr().js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function (haystack, needle) {
+  for (let i = 0; i < haystack.length; i++) {
+    const char = haystack[i];
+    if (char !== needle[0]) continue;
+
+    let concatedChar = "";
+    for (let j = 0; j < needle.length; j++) {
+      concatedChar += haystack[i + j];
+    }
+    if (concatedChar === needle) return i;
+  }
+
+  return -1;
+};
