2a515046ff99250d42baa368dc3c570f5a019233
eslamelkholy <55209776+eslamelkholy@users.noreply.github.com>
2022-10-12 20:01:04 +0200

    Max Consecutive Ones Optimized Solution

--- Learning/Array And Strings/14-Max Consecutive Ones.js
+++ Learning/Array And Strings/14-Max Consecutive Ones.js
@@ -18,3 +18,22 @@ var findMaxConsecutiveOnes = function (nums) {
 
   return maxSequence;
 };
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMaxConsecutiveOnes = function (nums) {
+  let count = 0;
+  let maxCount = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === 1) {
+      count++;
+    } else {
+      maxCount = Math.max(maxCount, count);
+      count = 0;
+    }
+  }
+  return Math.max(maxCount, count);
+};
