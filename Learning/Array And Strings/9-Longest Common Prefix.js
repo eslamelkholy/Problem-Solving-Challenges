16555d846866a96eb29ab6ae029054499cdd6dca
eslamelkholy <55209776+eslamelkholy@users.noreply.github.com>
2022-10-12 14:33:08 +0200

    Longest Common Prefix Vertical Scan

--- Learning/Array And Strings/9-Longest Common Prefix.js
+++ Learning/Array And Strings/9-Longest Common Prefix.js
@@ -28,3 +28,18 @@ var longestCommonPrefix = function (strs) {
   }
   return result;
 };
+
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function (strs) {
+  for (let i = 0; i < strs[0].length; i++) {
+    const str = strs[i];
+    for (let j = 1; j < strs.length; j++) {
+      if (strs[j][i] !== str[i]) return str.substring(0, i);
+    }
+  }
+
+  return strs[0];
+};
