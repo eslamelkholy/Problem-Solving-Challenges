90b39a45b251a6c520e6584d7dd620c53918af7c
eslamelkholy <55209776+eslamelkholy@users.noreply.github.com>
2022-10-14 14:26:24 +0200

    Reverse Words in String implement reverse Function

--- Learning/Array And Strings/19-Reverse Words in a String III.js
+++ Learning/Array And Strings/19-Reverse Words in a String III.js
@@ -14,3 +14,33 @@ var reverseWords = function (s) {
 
   return result;
 };
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function (s) {
+  const strArr = s.split(" ");
+  let result = reverseString(strArr[0]);
+
+  for (let i = 1; i < strArr.length; i++) {
+    result += " " + reverseString(strArr[i]);
+  }
+  return result;
+};
+
+const reverseString = function (s) {
+  let str = s.split("");
+  let leftPointer = 0;
+  let rightPointer = str.length - 1;
+
+  while (leftPointer < rightPointer) {
+    const temp = str[leftPointer];
+    str[leftPointer] = str[rightPointer];
+    str[rightPointer] = temp;
+
+    leftPointer++;
+    rightPointer--;
+  }
+  return str.join("");
+};
