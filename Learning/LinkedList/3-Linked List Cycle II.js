f1f997a8d288a9da7969b75a5d889a73fe4e4c28
eslamelkholy <55209776+eslamelkholy@users.noreply.github.com>
2022-10-17 21:22:13 +0200

    Linked List Cycle II Solution using Two Pointers

--- Learning/LinkedList/3-Linked List Cycle II.js
+++ Learning/LinkedList/3-Linked List Cycle II.js
@@ -23,3 +23,47 @@ var detectCycle = function (head) {
 
   return null;
 };
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+
+const isCycleExists = (head) => {
+  let slow = head;
+  let faster = head;
+
+  while (faster && faster.next) {
+    slow = slow.next;
+    faster = faster.next.next;
+
+    if (faster === slow) return slow;
+  }
+
+  return null;
+};
+
+var detectCycle = function (head) {
+  if (!head) return head;
+
+  const cycleIntersect = isCycleExists(head);
+  if (cycleIntersect === null) return null;
+
+  let pointer1 = head;
+  let pointer2 = cycleIntersect;
+
+  while (pointer1 !== pointer2) {
+    pointer1 = pointer1.next;
+    pointer2 = pointer2.next;
+  }
+
+  return pointer1;
+};
