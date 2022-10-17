274f5a26338efd348d72c55bc48958a4edb147fb
eslamelkholy <55209776+eslamelkholy@users.noreply.github.com>
2022-10-17 20:51:37 +0200

    Added Another Shitty Solution verified by Leetcode

--- Learning/LinkedList/2-Linked List Cycle.js
+++ Learning/LinkedList/2-Linked List Cycle.js
@@ -24,3 +24,31 @@ var hasCycle = function (head) {
 
   return false;
 };
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function (head) {
+  if (!head) return false;
+
+  let slow = head;
+  let faster = head.next;
+
+  while (faster !== slow) {
+    if (faster === null || faster.next === null) return false;
+
+    faster = faster.next.next;
+    slow = slow.next;
+  }
+
+  return true;
+};
