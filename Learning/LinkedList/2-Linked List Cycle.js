2d3ed6045d776b2edf0d59eac73d448c9f8d17a0
eslamelkholy <55209776+eslamelkholy@users.noreply.github.com>
2022-10-17 20:51:22 +0200

    Cycle Linkedlist Solution using Faster and Slower Pointer

---
+++ Learning/LinkedList/2-Linked List Cycle.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function (head) {
+  if (!head) return false;
+
+  let slow = head;
+  let faster = head.next;
+
+  while (faster && faster.next) {
+    if (faster === slow) return true;
+    faster = faster.next.next;
+    slow = slow.next;
+  }
+
+  return false;
+};
