e8ea89060689ca632583d0242acad2fc6e68829e
eslamelkholy <55209776+eslamelkholy@users.noreply.github.com>
2022-10-18 20:07:40 +0200

    Intersection of Two Linkedlist Using Hashtable O(N)

--- Learning/LinkedList/4-Intersection of Two Linked Lists.js
+++ Learning/LinkedList/4-Intersection of Two Linked Lists.js
@@ -26,3 +26,31 @@ var getIntersectionNode = function (headA, headB) {
   }
   return null;
 };
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+var getIntersectionNode = function (headA, headB) {
+  const nodeSet = new Set();
+  while (headA) {
+    nodeSet.add(headA);
+    headA = headA.next;
+  }
+
+  while (headB) {
+    if (nodeSet.has(headB)) return headB;
+
+    headB = headB.next;
+  }
+  return null;
+};
