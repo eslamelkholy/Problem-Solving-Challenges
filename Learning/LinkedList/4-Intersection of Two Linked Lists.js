f9535775bff6e13f67b267fc6a6c50ba7c63810f
eslamelkholy <55209776+eslamelkholy@users.noreply.github.com>
2022-10-18 20:11:02 +0200

    Intersection Using Two Pointer Technique

--- Learning/LinkedList/4-Intersection of Two Linked Lists.js
+++ Learning/LinkedList/4-Intersection of Two Linked Lists.js
@@ -54,3 +54,29 @@ var getIntersectionNode = function (headA, headB) {
   }
   return null;
 };
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+var getIntersectionNode = function (headA, headB) {
+  let tempA = headA;
+  let tempB = headB;
+
+  while (tempA !== tempB) {
+    tempA = tempA === null ? headB : tempA.next;
+
+    tempB = tempB === null ? headA : tempB.next;
+  }
+
+  return tempA;
+};
