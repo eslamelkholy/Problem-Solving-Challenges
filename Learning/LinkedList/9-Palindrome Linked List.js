7c8727fc50dfa899202eeac311823d1e2f6b81be
eslamelkholy <55209776+eslamelkholy@users.noreply.github.com>
2022-10-21 18:15:03 +0200

    Palindrom Linked list Solution

---
+++ Learning/LinkedList/9-Palindrome Linked List.js
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome = function (head) {
+  const arr = [];
+
+  while (head) {
+    arr.push(head.val);
+
+    head = head.next;
+  }
+
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left < right) {
+    if (arr[left] !== arr[right]) {
+      return false;
+    }
+
+    left++;
+    right--;
+  }
+
+  return true;
+};
