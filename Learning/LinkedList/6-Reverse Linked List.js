59f36cf851e17051b3fed9952ce9677d9f49e23a
eslamelkholy <55209776+eslamelkholy@users.noreply.github.com>
2022-10-21 18:23:24 +0200

    Reverse Linkedlist Solution

--- Learning/LinkedList/6-Reverse Linked List.js
+++ Learning/LinkedList/6-Reverse Linked List.js
@@ -25,3 +25,29 @@ var reverseList = function (head) {
 
   return prevNode;
 };
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function (head) {
+  let currentNode = head;
+  let prev = null;
+
+  while (currentNode) {
+    const temp = currentNode.next;
+
+    currentNode.next = prev;
+    prev = currentNode;
+
+    currentNode = temp;
+  }
+  return prev;
+};
