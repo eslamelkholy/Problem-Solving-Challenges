276dba143efbc49d397050e5a19912e7800522f4
eslamelkholy <55209776+eslamelkholy@users.noreply.github.com>
2022-10-21 18:04:56 +0200

    Odd Event Linkedlist Solution O(N)

---
+++ Learning/LinkedList/8-Odd Even Linked List.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var oddEvenList = function (head) {
+  if (head === null) return head;
+
+  let odd = head;
+  let even = head.next;
+  let evenHead = even;
+
+  while (even && even.next) {
+    odd.next = odd.next.next;
+    odd = odd.next;
+
+    even.next = even.next.next;
+    even = even.next;
+  }
+
+  odd.next = evenHead;
+
+  return head;
+};
