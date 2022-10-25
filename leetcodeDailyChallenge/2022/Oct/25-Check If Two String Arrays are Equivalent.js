9b4bcaca59498b63ea2791d4a6ba95ff55e68035
eslamelkholy <55209776+eslamelkholy@users.noreply.github.com>
2022-10-25 16:20:57 +0200

    Check if two string array Equals O(2N)

---
+++ leetcodeDailyChallenge/2022/Oct/25-Check If Two String Arrays are Equivalent.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string[]} word1
+ * @param {string[]} word2
+ * @return {boolean}
+ */
+var arrayStringsAreEqual = function (word1, word2) {
+  let str1 = "";
+  let str2 = "";
+  for (const char of word1) str1 += char;
+
+  for (const char of word2) str2 += char;
+
+  return str1 === str2;
+};
