db6309554385655eb367932212b4e0fad3e53123
eslamelkholy <55209776+eslamelkholy@users.noreply.github.com>
2022-12-09 19:32:24 +0200

    Search in BST take benefits of it's order

--- Learning/Recursion/4-Search in a Binary Search Tree.js
+++ Learning/Recursion/4-Search in a Binary Search Tree.js
@@ -26,3 +26,28 @@ var searchBST = function (root, val) {
 
   return result;
 };
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} val
+ * @return {TreeNode}
+ */
+var searchBST = function (root, val) {
+  while (root && val !== root.val) {
+    if (root.val < val) {
+      root = root.right;
+    } else {
+      root = root.left;
+    }
+  }
+
+  return root;
+};
