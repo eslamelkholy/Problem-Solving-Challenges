1ccd280a9f5129d2b10cd6689b4078bb89997805
eslamelkholy <55209776+eslamelkholy@users.noreply.github.com>
2022-12-09 20:13:49 +0200

    Climb Stairs using Recusion and Memoization

--- Learning/Recursion/7-Climbing Stairs.js
+++ Learning/Recursion/7-Climbing Stairs.js
@@ -13,3 +13,23 @@ const climb = (index, n) => {
 
   return climb(index + 1, n) + climb(index + 2, n);
 };
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function (n) {
+  const memo = {};
+  return climb(0, n, memo);
+};
+
+const climb = (index, n, memo) => {
+  if (index > n) return 0;
+
+  if (index === n) return 1;
+
+  if (memo[index] !== undefined) return memo[index];
+
+  memo[index] = climb(index + 1, n, memo) + climb(index + 2, n, memo);
+  return memo[index];
+};
