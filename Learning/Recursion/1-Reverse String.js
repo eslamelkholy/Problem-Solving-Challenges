8424b8d93f401b1b3729bb58595ac1328fdd75b2
eslamelkholy <55209776+eslamelkholy@users.noreply.github.com>
2022-12-09 15:27:35 +0200

    Reverse String Using Two Pointer Approach

---
+++ Learning/Recursion/1-Reverse String.js
@@ -0,0 +1,20 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function (s) {
+  let leftPointer = 0;
+  let rightPointer = s.length - 1;
+
+  while (leftPointer < rightPointer) {
+    const temp = s[leftPointer];
+
+    s[leftPointer] = s[rightPointer];
+    s[rightPointer] = temp;
+
+    leftPointer++;
+    rightPointer--;
+  }
+
+  return s;
+};
