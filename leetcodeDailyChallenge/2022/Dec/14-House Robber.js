2311c217270cf13bfe59c6ef5e2e9ca3197dd5de
eslamelkholy <55209776+eslamelkholy@users.noreply.github.com>
2022-12-14 17:50:54 +0200

    House Robber using DP Bottom up approach

---
+++ leetcodeDailyChallenge/2022/Dec/14-House Robber.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var rob = function (nums) {
+  const N = nums.length;
+  const dp = new Array(N).fill(0);
+  dp[0] = nums[0];
+  dp[1] = Math.max(nums[0], nums[1]);
+
+  for (let i = 2; i < N; i++) {
+    dp[i] += Math.max(nums[i] + dp[i - 2], dp[i - 1]);
+  }
+
+  return dp[N - 1];
+};
