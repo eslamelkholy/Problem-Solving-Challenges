bddc709ee68df9b05855e16d60d6d7e6d339853a
eslamelkholy <55209776+eslamelkholy@users.noreply.github.com>
2022-12-14 20:57:56 +0200

    Maximum Depth Optimized Space complexity non tail recursion

--- Learning/Recursion/8-Maximum Depth of Binary Tree.js
+++ Learning/Recursion/8-Maximum Depth of Binary Tree.js
@@ -25,3 +25,21 @@ var maxDepth = function (root) {
 
   return max;
 };
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function (root) {
+  if (!root) return 0;
+
+  return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+};
