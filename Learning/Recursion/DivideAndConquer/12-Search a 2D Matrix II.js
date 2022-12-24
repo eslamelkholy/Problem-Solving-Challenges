a177e0bc8abb2ffeb0567b4fd80e0748e85e10b2
eslamelkholy <55209776+eslamelkholy@users.noreply.github.com>
2022-12-24 20:06:35 +0200

    Search in 2D Matrix Binary search Brute Force

---
+++ Learning/Recursion/DivideAndConquer/12-Search a 2D Matrix II.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function (matrix, target) {
+  for (const nums of matrix) {
+    if (binarySearch(nums, target)) return true;
+  }
+
+  return false;
+};
+
+const binarySearch = (nums, target) => {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    if (nums[mid] === target) return true;
+
+    if (nums[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  return false;
+};
