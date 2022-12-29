8b9597b1fbb2af645af9491737a0d6b4e441a72b
eslamelkholy <55209776+eslamelkholy@users.noreply.github.com>
2022-12-29 22:27:26 +0200

    Permutations Using Backtrack andCaching

--- Learning/Recursion/Backtracking/22-Permutations.js
+++ Learning/Recursion/Backtracking/22-Permutations.js
@@ -25,3 +25,33 @@ const backtrack = (currentPerm, result, nums) => {
     currentPerm.pop();
   }
 };
+
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permute = function (nums) {
+  const result = [];
+
+  backtrack([], result, nums, new Set());
+
+  return result;
+};
+
+const backtrack = (currentPerm, result, nums, visited) => {
+  if (currentPerm.length === nums.length) {
+    return result.push([...currentPerm]);
+  }
+
+  for (let i = 0; i < nums.length; i++) {
+    if (visited.has(nums[i])) continue;
+
+    currentPerm.push(nums[i]);
+    visited.add(nums[i]);
+
+    backtrack(currentPerm, result, nums, visited);
+
+    visited.delete(nums[i]);
+    currentPerm.pop();
+  }
+};
