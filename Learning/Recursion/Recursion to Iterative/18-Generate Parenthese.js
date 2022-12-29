abdd51266d89749a41fcc06bdfa040bf8d974528
eslamelkholy <55209776+eslamelkholy@users.noreply.github.com>
2022-12-29 20:34:00 +0200

    Generate Parenthese Iterative Solution

--- Learning/Recursion/Recursion to Iterative/18-Generate Parenthese.js
+++ Learning/Recursion/Recursion to Iterative/18-Generate Parenthese.js
@@ -18,4 +18,25 @@ const backtrack = (leftRemain, rightRemain, current, result) => {
   if (rightRemain > 0) backtrack(leftRemain, rightRemain - 1, current + ")", result);
 };
 
+var generateParenthesis = function (n) {
+  const stack = [{ combination: "", left: n, right: n }];
+  const result = [];
+
+  while (stack.length > 0) {
+    const { combination, left, right } = stack.pop();
+    if (left > right) continue;
+
+    if (left > 0) {
+      stack.push({ combination: combination + "(", left: left - 1, right });
+    }
+    if (right > 0) {
+      stack.push({ combination: combination + ")", left, right: right - 1 });
+    }
+
+    if (left === 0 && right === 0) result.push(combination);
+  }
+
+  return result;
+};
+
 console.log(generateParenthesis(3));
