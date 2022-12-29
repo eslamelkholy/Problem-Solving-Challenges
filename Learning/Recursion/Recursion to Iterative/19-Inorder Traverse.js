41875b4927adc91ba2f0838167d1b4cb51417cba
eslamelkholy <55209776+eslamelkholy@users.noreply.github.com>
2022-12-29 21:41:25 +0200

    Inorder Traverse iterative Solution

--- Learning/Recursion/Recursion to Iterative/19-Inorder Traverse.js
+++ Learning/Recursion/Recursion to Iterative/19-Inorder Traverse.js
@@ -26,3 +26,34 @@ var inorderTraversal = function (root) {
 
   return result;
 };
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function (root) {
+  const result = [];
+  const stack = [];
+  let curr = root;
+
+  while (curr || stack.length > 0) {
+    while (curr) {
+      stack.push(curr);
+      curr = curr.left;
+    }
+
+    curr = stack.pop();
+    result.push(curr.val);
+    curr = curr.right;
+  }
+
+  return result;
+};
