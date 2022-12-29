0b48fcf5a5968f2e49b10eb20de69a7b06d5e535
eslamelkholy <55209776+eslamelkholy@users.noreply.github.com>
2022-12-29 21:49:51 +0200

    Binary Tree Level Order Traversal

---
+++ Learning/Recursion/Recursion to Iterative/20-Binary Tree Level Order Traversal.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrder = function (root) {
+  const queue = [root];
+  const result = [];
+  if (!root) return result;
+
+  while (queue.length > 0) {
+    const N = queue.length;
+    const currentLevel = [];
+
+    for (let i = 0; i < N; i++) {
+      const currentNode = queue.shift();
+      currentLevel.push(currentNode.val);
+
+      if (currentNode.left) queue.push(currentNode.left);
+      if (currentNode.right) queue.push(currentNode.right);
+    }
+
+    result.push(currentLevel);
+  }
+  return result;
+};
