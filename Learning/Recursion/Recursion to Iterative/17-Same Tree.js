40ef4449c493a631886c6e7335eb961d56465e38
eslamelkholy <55209776+eslamelkholy@users.noreply.github.com>
2022-12-29 19:46:04 +0200

    Same Tree Iterative Solution

--- Learning/Recursion/Recursion to Iterative/17-Same Tree.js
+++ Learning/Recursion/Recursion to Iterative/17-Same Tree.js
@@ -20,3 +20,39 @@ var isSameTree = function (p, q) {
 
   return isSameTree(p.right, q.right) && isSameTree(p.left, q.left);
 };
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+var isSameTree = function (p, q) {
+  const pStack = [p];
+  const qStack = [q];
+
+  while (pStack.length > 0) {
+    const currP = pStack.pop();
+    const currQ = qStack.pop();
+
+    if (!currP && !currQ) {
+      continue;
+    }
+
+    if (!currP || !currQ) return false;
+    if (currP.val !== currQ.val) return false;
+
+    pStack.push(currP.left);
+    qStack.push(currQ.left);
+    pStack.push(currP.right);
+    qStack.push(currQ.right);
+  }
+  return true;
+};
