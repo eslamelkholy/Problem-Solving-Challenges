047aa032b367982538d313bd70aeac13f54c6471
eslamelkholy <55209776+eslamelkholy@users.noreply.github.com>
2023-01-03 19:46:49 +0200

    Delete Columns to Make Sorted Clean Solution

--- leetcodeDailyChallenge/2033/Jan/3-Delete Columns to Make Sorted.js
+++ leetcodeDailyChallenge/2033/Jan/3-Delete Columns to Make Sorted.js
@@ -48,5 +48,22 @@ var minDeletionSize = function (strs) {
   return numOfDeleted;
 };
 
+/**
+ * @param {string[]} strs
+ * @return {number}
+ */
+var minDeletionSize = function (strs) {
+  let numOfDeleted = 0;
+  for (let i = 0; i < strs[0].length; i++) {
+    for (let j = 0; j < strs.length - 1; j++) {
+      if (strs[j][i] > strs[j + 1][i]) {
+        numOfDeleted++;
+        break;
+      }
+    }
+  }
+  return numOfDeleted;
+};
+
 // console.log(minDeletionSize(["rrjk", "furt", "guzm"]));
 console.log(minDeletionSize(["qowfc", "spyge", "sqbif", "vvrkk"]));
