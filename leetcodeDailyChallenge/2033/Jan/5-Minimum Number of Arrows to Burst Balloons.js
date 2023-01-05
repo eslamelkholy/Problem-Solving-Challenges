c4a88b4705fb29d9db1c0eeec6fd9119892485f1
eslamelkholy <55209776+eslamelkholy@users.noreply.github.com>
2023-01-05 17:43:22 +0200

    Minimum Number of Arrows to Burst Ballons

---
+++ leetcodeDailyChallenge/2033/Jan/5-Minimum Number of Arrows to Burst Balloons.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[][]} points
+ * @return {number}
+ */
+var findMinArrowShots = function (points) {
+  points.sort((a, b) => a[1] - b[1]);
+  let shots = 1;
+  let xEnd = points[0][1];
+
+  for (const [x, y] of points) {
+    if (x <= xEnd) {
+      continue;
+    } else {
+      xEnd = y;
+      shots++;
+    }
+  }
+
+  return shots;
+};
