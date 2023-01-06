717234e6bc487947379f535c1e530b21ec408204
eslamelkholy <55209776+eslamelkholy@users.noreply.github.com>
2023-01-06 13:30:44 +0200

    Maximum Ice Cream Bars

--- leetcodeDailyChallenge/2033/Jan/6-Maximum Ice Cream Bars.js
+++ leetcodeDailyChallenge/2033/Jan/6-Maximum Ice Cream Bars.js
@@ -35,3 +35,22 @@ var maxIceCream = function (costs, coins) {
 
   return iceCreams;
 };
+
+/**
+ * @param {number[]} costs
+ * @param {number} coins
+ * @return {number}
+ */
+var maxIceCream = function (costs, coins) {
+  costs.sort((a, b) => a - b);
+  let iceCreams = 0;
+
+  for (const cost of costs) {
+    if (cost > coins) break;
+
+    iceCreams++;
+    coins -= cost;
+  }
+
+  return iceCreams;
+};
