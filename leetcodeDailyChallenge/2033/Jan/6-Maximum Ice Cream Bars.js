34c9c1a0f58e0d0eb0429475ccb58f585de92ab1
eslamelkholy <55209776+eslamelkholy@users.noreply.github.com>
2023-01-06 13:28:30 +0200

    Maximum Ice Cream Bars Solution

---
+++ leetcodeDailyChallenge/2033/Jan/6-Maximum Ice Cream Bars.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} costs
+ * @param {number} coins
+ * @return {number}
+ */
+var maxIceCream = function (costs, coins) {
+  costs.sort((a, b) => a - b);
+  let iceCreams = 0;
+
+  for (const cost of costs) {
+    if (coins === 0) break;
+    if (coins - cost < 0) continue;
+
+    iceCreams++;
+    coins -= cost;
+  }
+  return iceCreams;
+};
