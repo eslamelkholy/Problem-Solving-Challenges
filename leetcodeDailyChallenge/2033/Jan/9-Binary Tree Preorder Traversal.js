8f4973f9ca99501361daa85ea19509554e9ad864
eslamelkholy <55209776+eslamelkholy@users.noreply.github.com>
2023-01-09 17:14:32 +0200

    Preorder Traverse for Binary Tree

--- leetcodeDailyChallenge/2033/Jan/9-Binary Tree Preorder Traversal.js
+++ leetcodeDailyChallenge/2033/Jan/9-Binary Tree Preorder Traversal.js
@@ -23,3 +23,32 @@ var preorderTraversal = function (root) {
 
   return nums;
 };
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var preorderTraversal = function (root) {
+  const nums = [];
+  const queue = [root];
+  if (!root) return nums;
+
+  while (queue.length > 0) {
+    const currentNode = queue.shift();
+
+    nums.push(currentNode.val);
+
+    if (currentNode.right) queue.unshift(currentNode.right);
+    if (currentNode.left) queue.unshift(currentNode.left);
+  }
+
+  return nums;
+};
