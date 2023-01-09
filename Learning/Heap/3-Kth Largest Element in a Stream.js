70d1f1f851f6a608d43b20e7e1e2c9c7e883a25a
eslamelkholy <55209776+eslamelkholy@users.noreply.github.com>
2023-01-09 19:11:48 +0200

    kth Largest Element using Min Heap

--- Learning/Heap/3-Kth Largest Element in a Stream.js
+++ Learning/Heap/3-Kth Largest Element in a Stream.js
@@ -31,3 +31,34 @@ KthLargest.prototype.add = function (val) {
  * var obj = new KthLargest(k, nums)
  * var param_1 = obj.add(val)
  */
+
+/**
+ * @param {number} k
+ * @param {number[]} nums
+ */
+var KthLargest = function (k, nums) {
+  this.k = k;
+  this.mpq = new MinPriorityQueue({ priority: (val) => val });
+
+  nums.forEach((num) => this.mpq.enqueue(num));
+  while (this.mpq.size() > k) {
+    this.mpq.dequeue();
+  }
+};
+
+/**
+ * @param {number} val
+ * @return {number}
+ */
+KthLargest.prototype.add = function (val) {
+  this.mpq.enqueue(val);
+
+  while (this.mpq.size() > this.k) this.mpq.dequeue();
+
+  return this.mpq.front().element;
+};
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * var obj = new KthLargest(k, nums)
+ * var param_1 = obj.add(val)
+ */
