72f6bc8f21b4c0a24fb793da2c814b1fb4d9be02
eslamelkholy <55209776+eslamelkholy@users.noreply.github.com>
2023-01-10 14:35:28 +0200

    Last Stone Weight Using Heap

--- Learning/Heap/4-Last Stone Weight.js
+++ Learning/Heap/4-Last Stone Weight.js
@@ -20,3 +20,32 @@ var lastStoneWeight = function (stones) {
 
   return stones;
 };
+
+/**
+ * @param {number[]} stones
+ * @return {number}
+ */
+var lastStoneWeight = function (stones) {
+  /**
+   * Rules:
+   * 1- x === y >> Both Gone
+   * 2- x !== y >> x Gone and y = y - x  (x <= y)
+   */
+  const mpq = new MaxPriorityQueue();
+  stones.forEach((stone) => mpq.enqueue(stone));
+
+  while (mpq.size() > 1) {
+    const x = mpq.dequeue().element;
+    const y = mpq.dequeue().element;
+
+    if (x === y) continue;
+
+    mpq.enqueue(x - y);
+  }
+
+  return [mpq.dequeue()?.element];
+};
+
+console.log(lastStoneWeight([2, 7, 4, 1, 8, 1]));
+console.log(lastStoneWeight([1]));
+console.log(lastStoneWeight([2, 2]));
