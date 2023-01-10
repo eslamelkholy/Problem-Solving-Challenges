2f5b12dcbe6dfa0d101b196d583cbad829df761c
eslamelkholy <55209776+eslamelkholy@users.noreply.github.com>
2023-01-10 20:08:10 +0200

    Minimum cost to connect Sticks using Heap

--- Learning/Heap/9-Minimum Cost to Connect Sticks.js
+++ Learning/Heap/9-Minimum Cost to Connect Sticks.js
@@ -16,3 +16,25 @@ var connectSticks = function (sticks) {
 
   return connected.reduce((a, b) => a + b, 0);
 };
+
+/**
+ * @param {number[]} sticks
+ * @return {number}
+ */
+var connectSticks = function (sticks) {
+  const minPQ = new MinPriorityQueue({ priority: (stick) => stick });
+  sticks.forEach((stick) => minPQ.enqueue(stick));
+
+  let sum = 0;
+
+  while (minPQ.size() > 1) {
+    const s1 = minPQ.dequeue().element;
+    const s2 = minPQ.dequeue().element;
+    const total = s1 + s2;
+    minPQ.enqueue(total);
+
+    sum += s1 + s2;
+  }
+
+  return sum;
+};
