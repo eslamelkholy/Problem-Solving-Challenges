7fc5e16ab7f758c355e9fa8a8f628aa171ad579f
eslamelkholy <55209776+eslamelkholy@users.noreply.github.com>
2023-01-13 16:02:08 +0200

    Find Median Using To Heaps to get middle elements Min/Max Heap

--- Learning/Heap/11-Find Median from Data Stream.js
+++ Learning/Heap/11-Find Median from Data Stream.js
@@ -29,6 +29,48 @@ class MedianFinder {
  * obj.addNum(num)
  * var param_2 = obj.findMedian()
  */
+
+const { MinPriorityQueue, MaxPriorityQueue } = require("@datastructures-js/priority-queue");
+class MedianFinder {
+  constructor() {
+    this.minPQ = new MinPriorityQueue({ priority: (num) => num });
+    this.maxPQ = new MaxPriorityQueue({ priority: (num) => num });
+  }
+  /**
+   * @param {number} num
+   * @return {void}
+   */
+  addNum(num) {
+    this.maxPQ.enqueue(num); // 1- Add it to Max
+
+    this.minPQ.enqueue(this.maxPQ.front().element); // 2- Get max Value add it To Min
+
+    this.maxPQ.dequeue(); // 3- Remove it From Max
+
+    if (this.maxPQ.size() < this.minPQ.size()) {
+      // 4-Balance Size Priority
+      this.maxPQ.enqueue(this.minPQ.front().element);
+      this.minPQ.dequeue();
+    }
+  }
+  /**
+   * @return {number}
+   */
+  findMedian() {
+    if (this.maxPQ.size() > this.minPQ.size()) {
+      return this.maxPQ.front().element;
+    }
+    return (this.maxPQ.front().element + this.minPQ.front().element) * 0.5;
+  }
+}
+
+const medianFinder = new MedianFinder();
+console.log(medianFinder.addNum(1)); // arr = [1]
+console.log(medianFinder.addNum(2)); // arr = [1, 2]
+console.log(medianFinder.findMedian()); // return 1.5 (i.e., (1 + 2) / 2)
+console.log(medianFinder.addNum(3)); // arr[1, 2, 3]
+console.log(medianFinder.findMedian()); // return 2.0
+
 const medianFinder = new MedianFinder();
 console.log(medianFinder.addNum(1)); // arr = [1]
 console.log(medianFinder.addNum(2)); // arr = [1, 2]
