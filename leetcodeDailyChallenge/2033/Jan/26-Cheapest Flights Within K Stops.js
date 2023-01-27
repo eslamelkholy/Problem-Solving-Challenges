eced15a2a29abdecf3c37127845869502f472a71
eslamelkholy <55209776+eslamelkholy@users.noreply.github.com>
2023-01-27 16:46:55 +0200

    Cheapest Flights Within K Stops using Graph and Heap

--- leetcodeDailyChallenge/2033/Jan/26-Cheapest Flights Within K Stops.js
+++ leetcodeDailyChallenge/2033/Jan/26-Cheapest Flights Within K Stops.js
@@ -38,6 +38,47 @@ const buildGraph = (flights) => {
   }
   return graph;
 };
+/**
+ * @param {number} n
+ * @param {number[][]} flights
+ * @param {number} src
+ * @param {number} dst
+ * @param {number} k
+ * @return {number}
+ */
+var findCheapestPrice = function (n, flights, src, dst, k) {
+  const graph = buildGraph(flights);
+  const stops = new Array(n).fill(Infinity);
+  const pq = new MinPriorityQueue({ priority: (val) => val.cost });
+  pq.enqueue({ cost: 0, source: src, steps: 0 }); // cost, src, steps
+
+  while (pq.size() > 0) {
+    const { cost, source, steps } = pq.dequeue().element;
+
+    if (steps > stops[source] || steps > k + 1) continue; // Validate Steps
+
+    stops[source] = steps;
+
+    if (source === dst) return cost;
+
+    if (graph[source] === undefined) continue;
+    for (const [nextCity, nextCost] of graph[source]) {
+      pq.enqueue({ cost: cost + nextCost, source: nextCity, steps: steps + 1 });
+    }
+  }
+
+  return -1;
+};
+
+const buildGraph = (flights) => {
+  const graph = {};
+  for (const [start, end, cost] of flights) {
+    if (graph[start] === undefined) graph[start] = [];
+
+    graph[start].push([end, cost]);
+  }
+  return graph;
+};
 
 console.log(
   findCheapestPrice(
