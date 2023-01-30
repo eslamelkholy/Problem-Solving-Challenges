19c90f3a849292a5d29644ca7d06fb43e5e99e0f
eslamelkholy <55209776+eslamelkholy@users.noreply.github.com>
2023-01-30 12:27:41 +0200

    N-th Tribonacci Number Memoization Solution

--- leetcodeDailyChallenge/2033/Jan/30-N-th Tribonacci Number.js
+++ leetcodeDailyChallenge/2033/Jan/30-N-th Tribonacci Number.js
@@ -8,3 +8,21 @@ var tribonacci = function (n) {
 
   return tribonacci(n - 1) + tribonacci(n - 2) + tribonacci(n - 3);
 };
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var tribonacci = function (n) {
+  return calculate(n, {});
+};
+
+const calculate = (n, memo) => {
+  if (n === 0) return 0;
+  if (n === 1 || n === 2) return 1;
+  if (memo[n] !== undefined) return memo[n];
+
+  memo[n] = calculate(n - 1, memo) + calculate(n - 2, memo) + calculate(n - 3, memo);
+
+  return memo[n];
+};
