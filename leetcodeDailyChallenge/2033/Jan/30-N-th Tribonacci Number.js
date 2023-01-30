aea984aa2ef5d03eaa4f505e46de7dc0f268ffdd
eslamelkholy <55209776+eslamelkholy@users.noreply.github.com>
2023-01-30 12:29:56 +0200

    Tribonacci Number using DP Solution

--- leetcodeDailyChallenge/2033/Jan/30-N-th Tribonacci Number.js
+++ leetcodeDailyChallenge/2033/Jan/30-N-th Tribonacci Number.js
@@ -26,3 +26,19 @@ const calculate = (n, memo) => {
 
   return memo[n];
 };
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var tribonacci = function (n) {
+  const dp = new Array(n).fill(0);
+  dp[0] = 0;
+  dp[1] = 1;
+  dp[2] = 1;
+
+  for (let i = 3; i <= n; i++) {
+    dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+  }
+  return dp[n];
+};
