17b40f1972445b0004e5f6fd8a5196e789fd2447
eslamelkholy <55209776+eslamelkholy@users.noreply.github.com>
2023-02-20 00:07:59 +0200

    Binary Tree Zigzag Optimized Solution

--- leetcodeDailyChallenge/2033/Feb/19-Binary Tree Zigzag Level Order Traversal.js
+++ leetcodeDailyChallenge/2033/Feb/19-Binary Tree Zigzag Level Order Traversal.js
@@ -34,3 +34,40 @@ var zigzagLevelOrder = function (root) {
   }
   return result;
 };
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var zigzagLevelOrder = function (root) {
+  const queue = [root];
+  const result = [];
+  let reverse = false;
+
+  if (!root) return result;
+
+  while (queue.length > 0) {
+    const currentLevelValues = [];
+    const N = queue.length;
+
+    for (let i = 0; i < N; i++) {
+      const current = queue.shift();
+
+      reverse ? currentLevelValues.unshift(current.val) : currentLevelValues.push(current.val);
+
+      if (current.left) queue.push(current.left);
+      if (current.right) queue.push(current.right);
+    }
+    result.push(currentLevelValues);
+    reverse = !reverse;
+  }
+  return result;
+};
