90bbf5540c841f9846dc9a1a052ec1e70872fd8f
eslamelkholy <eslamelkholy5325762@gmail.com>
2023-05-13 17:54:58 +0300

    Binary Tree Path Sum Cleared Solution

--- CodingPreparation/binaryTree/7-Path Sum.js
+++ CodingPreparation/binaryTree/7-Path Sum.js
@@ -31,3 +31,12 @@ var hasPathSum = function (root, targetSum) {
 
   return hasPath;
 };
+
+var hasPathSum = function (root, targetSum) {
+  if (!root) return false;
+
+  targetSum -= root.val;
+  if (targetSum === 0 && !root.left && !root.right) return true;
+
+  return hasPathSum(root.left, targetSum) || hasPathSum(root.right, targetSum);
+};
