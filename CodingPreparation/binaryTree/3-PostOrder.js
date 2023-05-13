a18037f32bfcdff16b7130843e611abab7cf9e40
eslamelkholy <55209776+eslamelkholy@users.noreply.github.com>
2023-05-13 16:06:28 +0300

    Post order Traverse

---
+++ CodingPreparation/binaryTree/3-PostOrder.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var postorderTraversal = function (root) {
+  const result = [];
+  const postorder = (root) => {
+    if (!root) return;
+
+    postorder(root.left);
+
+    postorder(root.right);
+
+    result.push(root.val);
+  };
+  postorder(root);
+
+  return result;
+};
