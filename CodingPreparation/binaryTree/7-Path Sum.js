41cdcd99c7c836075391ede3f3f6a5936da0f0c9
eslamelkholy <eslamelkholy5325762@gmail.com>
2023-05-13 17:51:54 +0300

    Binary Tree Path SUm Solution

---
+++ CodingPreparation/binaryTree/7-Path Sum.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {boolean}
+ */
+var hasPathSum = function (root, targetSum) {
+  let hasPath = false;
+
+  const findPath = (root, currentSum) => {
+    if (!root) return false;
+
+    const totalSum = currentSum + root.val;
+
+    if (totalSum === targetSum && !root.left && !root.right) {
+      hasPath = true;
+    }
+
+    findPath(root.left, totalSum);
+    findPath(root.right, totalSum);
+  };
+
+  findPath(root, 0);
+
+  return hasPath;
+};
