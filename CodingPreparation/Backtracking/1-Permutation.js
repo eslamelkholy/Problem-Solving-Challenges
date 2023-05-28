1521ce9cbe15c9f91c5b37b1c1dc75651404bd9d
eslamelkholy <eslamelkholy5325762@gmail.com>
2023-05-28 16:51:11 +0300

    Permutations Optimized Solution using Backtrack

--- CodingPreparation/Backtracking/1-Permutation.js
+++ CodingPreparation/Backtracking/1-Permutation.js
@@ -1,3 +1,31 @@
+// /**
+//  * @param {number[]} nums
+//  * @return {number[][]}
+//  */
+// var permute = function (nums) {
+//   const result = [];
+
+//   backtrack(nums, result, []);
+
+//   return result;
+// };
+
+// const backtrack = (nums, result, currentPermute) => {
+//   if (currentPermute.length === nums.length) {
+//     return result.push(currentPermute);
+//   }
+
+//   for (const num of nums) {
+//     if (currentPermute.includes(num)) continue;
+
+//     currentPermute.push(num);
+
+//     backtrack(nums, result, [...currentPermute]);
+
+//     currentPermute.pop();
+//   }
+// };
+
 /**
  * @param {number[]} nums
  * @return {number[][]}
@@ -5,23 +33,25 @@
 var permute = function (nums) {
   const result = [];
 
-  backtrack(nums, result, []);
+  backtrack(nums, result, [], new Array(nums.length).fill(false));
 
   return result;
 };
 
-const backtrack = (nums, result, currentPermute) => {
+const backtrack = (nums, result, currentPermute, visited) => {
   if (currentPermute.length === nums.length) {
-    return result.push(currentPermute);
+    return result.push([...currentPermute]);
   }
 
-  for (const num of nums) {
-    if (currentPermute.includes(num)) continue;
+  for (let i = 0; i < nums.length; i++) {
+    if (visited[i] === true) continue;
 
-    currentPermute.push(num);
+    currentPermute.push(nums[i]);
+    visited[i] = true;
 
-    backtrack(nums, result, [...currentPermute]);
+    backtrack(nums, result, currentPermute, visited);
 
+    visited[i] = false;
     currentPermute.pop();
   }
 };
