c2aeb0aff6fe33d05ae172a65fa0233033f0f3d5
eslamelkholy <eslamelkholy5325762@gmail.com>
2023-08-12 17:00:10 +0300

    Reverse Linked List

---
+++ CodingPreparation/playground/recursion/2-Reverse Linked List.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function (head) {
+  let currentNode = head;
+  let prev = null;
+
+  while (currentNode) {
+    const cacheNext = currentNode.next;
+
+    currentNode.next = prev;
+    prev = currentNode;
+
+    currentNode = cacheNext;
+  }
+
+  return prev;
+};
