20dfcdb62393d9ef64862c4b4972c91c06cdf94f
eslamelkholy <eslamelkholy5325762@gmail.com>
2024-05-08 14:26:04 +0100

    Solve Relative ranks using Max Priority queue

--- leetcodeDailyChallenge/2024/May/8-Relative Ranks.js
+++ leetcodeDailyChallenge/2024/May/8-Relative Ranks.js
@@ -1,3 +1,4 @@
+const { MaxPriorityQueue } = require("@datastructures-js/priority-queue");
 /**
  * @param {number[]} score
  * @return {string[]}
@@ -28,5 +29,39 @@ var findRelativeRanks = function (score) {
         output.push(place + "");
     }
   }
+  return output;
+};
+
+/**
+ * @param {number[]} score
+ * @return {string[]}
+ */
+var findRelativeRanks = function (score) {
+  const maxPQ = new MaxPriorityQueue({ priority: (diff) => diff.priority });
+  const output = new Array(score.length);
+
+  for (let i = 0; i < score.length; i++) {
+    maxPQ.enqueue({ num: i, priority: score[i] });
+  }
+
+  let place = 1;
+  while (!maxPQ.isEmpty()) {
+    const { num: index } = maxPQ.dequeue().element;
+    switch (place) {
+      case 1:
+        output[index] = "Gold Medal";
+        break;
+      case 2:
+        output[index] = "Silver Medal";
+        break;
+      case 3:
+        output[index] = "Bronze Medal";
+        break;
+      default:
+        output[index] = place + "";
+    }
+    place++;
+  }
+
   return output;
 };
