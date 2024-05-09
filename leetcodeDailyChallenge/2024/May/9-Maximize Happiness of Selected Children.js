8672905a647ee039b702902f2fd4bf16e7d1db00
eslamelkholy <eslamelkholy5325762@gmail.com>
2024-05-09 12:35:22 +0100

    Maximum Happiness of Childrens

---
+++ leetcodeDailyChallenge/2024/May/9-Maximize Happiness of Selected Children.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} happiness
+ * @param {number} k
+ * @return {number}
+ */
+var maximumHappinessSum = function (happiness, k) {
+  happiness.sort((a, b) => b - a);
+  let max = 0;
+
+  let rounds = 0;
+  while (k > 0) {
+    const happinessSum = happiness[rounds] - rounds;
+    if (happinessSum > 0) max += happiness[rounds] - rounds;
+
+    rounds++;
+    k--;
+  }
+
+  return max;
+};
