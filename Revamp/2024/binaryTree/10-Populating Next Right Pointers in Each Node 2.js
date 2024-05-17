b8e7700075869a2ae4b6683c3865f587cb69d4de
eslamelkholy <eslamelkholy5325762@gmail.com>
2024-05-17 22:27:51 +0100

    Populating next right pointer || in binary tree

---
+++ Revamp/2024/binaryTree/10-Populating Next Right Pointers in Each Node 2.js
@@ -0,0 +1,34 @@
+/**
+ * // Definition for a Node.
+ * function Node(val, left, right, next) {
+ *    this.val = val === undefined ? null : val;
+ *    this.left = left === undefined ? null : left;
+ *    this.right = right === undefined ? null : right;
+ *    this.next = next === undefined ? null : next;
+ * };
+ */
+
+/**
+ * @param {Node} root
+ * @return {Node}
+ */
+var connect = function (root) {
+  if (!root) return null;
+  const queue = [root];
+
+  while (queue.length > 0) {
+    const N = queue.length; // Current size of N
+    for (let i = 0; i < N; i++) {
+      const currentNode = queue.shift();
+
+      currentNode.next = i === N - 1 ? null : queue[0];
+
+      if (currentNode.left) queue.push(currentNode.left);
+      if (currentNode.right) queue.push(currentNode.right);
+    }
+  }
+
+  return root;
+};
+
+// Note this algorithm is like Level order traverse but with some edits
