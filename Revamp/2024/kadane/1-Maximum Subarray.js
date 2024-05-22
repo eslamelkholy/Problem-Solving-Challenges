ae41f1c2f526cb635a97ad6680a27084e255f85d
eslamelkholy <eslamelkholy5325762@gmail.com>
2024-05-22 11:17:44 +0100

    More cleaner solution for kadane's

--- Revamp/2024/kadane/1-Maximum Subarray.js
+++ Revamp/2024/kadane/1-Maximum Subarray.js
@@ -19,4 +19,21 @@ var maxSubArray = function (nums) {
   return maxSum;
 };
 
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function (nums) {
+  let currentMax = 0;
+  let maxSum = nums[0];
+
+  for (const num of nums) {
+    currentMax = Math.max(num, currentMax + num);
+
+    maxSum = Math.max(maxSum, currentMax);
+  }
+
+  return maxSum;
+};
+
 console.log(maxSubArray((nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4])));
