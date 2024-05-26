5bf0f2dcc427d77685d63d828c53f8f00e4dd096
eslamelkholy <eslamelkholy5325762@gmail.com>
2024-05-26 23:09:34 +0100

    Container with most water using two pointers

--- Revamp/2024/Two Pointers/4-Container With Most Water.js
+++ Revamp/2024/Two Pointers/4-Container With Most Water.js
@@ -14,3 +14,23 @@ var maxArea = function (height) {
 
   return max;
 };
+
+var maxArea = function (height) {
+  let max = 0;
+  let left = 0;
+  let right = height.length - 1;
+
+  while (left < right) {
+    let width = right - left;
+
+    max = Math.max(max, Math.min(height[left], height[right]) * width);
+
+    if (height[left] < height[right]) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+
+  return max;
+};
