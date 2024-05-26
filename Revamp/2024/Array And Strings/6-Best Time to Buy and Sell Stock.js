e7cf8ff55564780368baee9f0a4c201bf57233b9
eslamelkholy <eslamelkholy5325762@gmail.com>
2024-05-26 21:44:41 +0100

    Best time to buy and sell stock solution TLE

---
+++ Revamp/2024/Array And Strings/6-Best Time to Buy and Sell Stock.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function (prices) {
+  let max = 0;
+
+  for (let i = 0; i < prices.length; i++) {
+    for (let j = i + 1; j < prices.length; j++) {
+      const profit = prices[j] - prices[i];
+      max = Math.max(max, profit);
+    }
+  }
+
+  return max;
+};
