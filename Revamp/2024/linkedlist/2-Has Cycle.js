a9b789a8ef207ea7cf14827cc534c55a6d90d1c9
eslamelkholy <eslamelkholy5325762@gmail.com>
2024-05-29 14:25:37 +0100

    Has Cycle Straightforward solution

--- Revamp/2024/linkedlist/2-Has Cycle.js
+++ Revamp/2024/linkedlist/2-Has Cycle.js
@@ -11,6 +11,33 @@
  * @return {boolean}
  */
 var hasCycle = function (head) {
+  const visited = new Set();
+
+  while (head) {
+    if (visited.has(head)) {
+      return true;
+    }
+    visited.add(head);
+
+    head = head.next;
+  }
+
+  return false;
+};
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function (head) {
   if (!head) return false;
 
   let slow = head;
