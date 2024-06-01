c0907a195c21348e9003ee9b6db13f71797d9c71
eslamelkholy <eslamelkholy5325762@gmail.com>
2024-06-01 20:17:35 +0100

    Sum root to lead numbers

---
+++ Revamp/2024/binaryTree/16-Sum Root to Leaf Numbers.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var sumNumbers = function (root) {
+  const nums = [];
+
+  const dfs = (root, path) => {
+    if (!root) return null;
+
+    path.push(root.val);
+
+    if (!root.left && !root.right) {
+      return nums.push(path);
+    }
+
+    dfs(root.left, [...path]);
+    dfs(root.right, [...path]);
+  };
+
+  dfs(root, []);
+
+  return nums.reduce((partialSum, a) => partialSum + Number(a.join("")), 0);
+};
