b2bc4e8aaabf1f5275ae810f7fd39dc05b820ca9
eslamelkholy <eslamelkholy5325762@gmail.com>
2024-06-01 14:26:19 +0100

    Find first and last position of element Using binary search

---
+++ Revamp/2024/Binar Search/6-Find First and Last Position of Element in Sorted Array.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var searchRange = function (nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+  let found = false;
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+
+    if (nums[mid] === target) {
+      found = true;
+    }
+
+    if (nums[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+  if (!found) return [-1, -1];
+
+  let end = left;
+  for (let i = left; i < nums.length; i++) {
+    if (nums[i] !== target) {
+      end = i - 1;
+      break;
+    }
+    end = i;
+  }
+
+  return [left, end];
+};
