d5a35a5cec829fd49d8187f3c71f2a6c68b6c662
eslamelkholy <eslamelkholy5325762@gmail.com>
2024-06-01 20:04:59 +0100

    Path sum solution

--- Revamp/2024/binaryTree/8-Path Sum.js
+++ Revamp/2024/binaryTree/8-Path Sum.js
@@ -28,3 +28,35 @@ var hasPathSum = function (root, targetSum) {
 
   return hasPath;
 };
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {boolean}
+ */
+var hasPathSum = function (root, targetSum) {
+  let exists = false;
+
+  const dfs = (root, sum) => {
+    if (!root) return sum;
+
+    sum += root.val;
+    if (sum === targetSum && !root.left && !root.right) {
+      return (exists = true);
+    }
+
+    dfs(root.left, sum);
+    dfs(root.right, sum);
+  };
+  dfs(root, 0);
+
+  return exists;
+};
