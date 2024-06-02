850a0d1539076ba64ca0bdd06ff6af5765841022
eslamelkholy <eslamelkholy5325762@gmail.com>
2024-06-02 14:04:00 +0100

    Word break solution using sets

--- Revamp/2024/hashtable/14-Word Break.js
+++ Revamp/2024/hashtable/14-Word Break.js
@@ -10,10 +10,10 @@ var wordBreak = function (s, wordDict) {
 
   while (queue.length > 0) {
     const start = queue.shift();
-
     if (start === s.length) return true;
 
     for (let end = start + 1; end <= s.length; end++) {
+      // <= because of substring
       if (seen.has(end)) continue;
 
       if (words.has(s.substring(start, end))) {
