24f42d3ae87e429a7bac1ff371b9f94c3ecbdbcd
eslamelkholy <eslamelkholy5325762@gmail.com>
2024-06-02 13:33:06 +0100

    Climb stairs using memoization

--- Revamp/2024/DP/1-Climbing Stairs.js
+++ Revamp/2024/DP/1-Climbing Stairs.js
@@ -13,3 +13,24 @@ const climb = (n, stairs) => {
 
   return climb(n, stairs + 1) + climb(n, stairs + 2);
 };
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function (n) {
+  const memo = {};
+  return climb2(n, 0, memo);
+};
+
+const climb2 = (n, stairs, memo) => {
+  if (memo[stairs] !== undefined) return memo[stairs];
+
+  if (stairs > n) return 0;
+
+  if (stairs === n) return 1;
+
+  memo[stairs] = climb(n, stairs + 1, memo) + climb(n, stairs + 2, memo);
+
+  return memo[stairs];
+};
