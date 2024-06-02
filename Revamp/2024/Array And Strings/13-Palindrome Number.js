f01cb5c1ecd282cadf9f555dde4228ed8cc6690d
eslamelkholy <eslamelkholy5325762@gmail.com>
2024-06-02 13:22:26 +0100

    Palindrom number solution

---
+++ Revamp/2024/Array And Strings/13-Palindrome Number.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function (x) {
+  const str = String(x);
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] !== str[str.length - 1 - i]) return false;
+  }
+
+  return true;
+};
