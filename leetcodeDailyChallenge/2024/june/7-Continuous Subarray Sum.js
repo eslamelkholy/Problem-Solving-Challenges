9d02956e0ae7b3a0849fc5ddf2d2dfd40275ec5e
eslamelkholy <eslamelkholy5325762@gmail.com>
2024-06-08 13:54:49 +0100

    Continuous subarray sum using prefix mod

--- leetcodeDailyChallenge/2024/june/7-Continuous Subarray Sum.js
+++ leetcodeDailyChallenge/2024/june/7-Continuous Subarray Sum.js
@@ -14,3 +14,25 @@ var checkSubarraySum = function (nums, k) {
 
   return false;
 };
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var checkSubarraySum = function (nums, k) {
+  let prefixMod = 0;
+  let modSeen = { 0: -1 };
+
+  for (let i = 0; i < nums.length; i++) {
+    prefixMod = (prefixMod + nums[i]) % k;
+
+    if (modSeen[prefixMod] !== undefined) {
+      if (i - modSeen[prefixMod] > 1) return true;
+    } else {
+      modSeen[prefixMod] = i;
+    }
+  }
+
+  return false;
+};
