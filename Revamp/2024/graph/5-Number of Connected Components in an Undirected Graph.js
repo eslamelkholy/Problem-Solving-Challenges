1aef5f6f75eb5c2a4fd83a8b19b935637b40baae
eslamelkholy <eslamelkholy5325762@gmail.com>
2024-06-08 14:17:00 +0100

    Number of connected components using Graph

--- Revamp/2024/graph/5-Number of Connected Components in an Undirected Graph.js
+++ Revamp/2024/graph/5-Number of Connected Components in an Undirected Graph.js
@@ -6,56 +6,37 @@
 var countComponents = function (n, edges) {
   const graph = buildGraph(edges, n);
   const visited = new Set();
-  let count = 0;
+  let sum = 0;
 
-  for (const node in graph) {
-    count += dfs(Number(node), graph, visited);
+  for (let i = 0; i < n; i++) {
+    if (graph[i] === undefined) continue;
+
+    if (hasPath(graph, visited, i)) sum++;
   }
 
-  return count;
+  return sum;
 };
 
-const dfs = (node, graph, visited) => {
-  if (visited.has(node)) return 0;
+const hasPath = (graph, visited, node) => {
+  if (visited.has(node)) return false;
 
   visited.add(node);
 
   for (const neighbor of graph[node]) {
-    dfs(neighbor, graph, visited);
+    hasPath(graph, visited, neighbor);
   }
 
-  return 1;
+  return true;
 };
 
-const buildGraph = (edges, n) => {
+const buildGraph = (edges, N) => {
   const graph = {};
-
-  for (let i = 0; i < n; i++) {
-    graph[i] = [];
-  }
+  for (let i = 0; i < N; i++) graph[i] = [];
 
   for (const [x, y] of edges) {
     graph[x].push(y);
     graph[y].push(x);
   }
+
   return graph;
 };
-
-console.log(
-  countComponents(
-    (n = 5),
-    (edges = [
-      [0, 1],
-      [1, 2],
-      [3, 4],
-    ])
-  )
-);
-
-console.log(
-  countComponents(4, [
-    [2, 3],
-    [1, 2],
-    [1, 3],
-  ])
-);
