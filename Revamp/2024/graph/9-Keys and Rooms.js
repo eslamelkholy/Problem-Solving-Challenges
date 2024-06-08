6d6bf9ba8b03ea42409163c362163b25060503f1
eslamelkholy <eslamelkholy5325762@gmail.com>
2024-06-08 20:29:51 +0100

    Keys and rooms solution using DFS

--- Revamp/2024/graph/9-Keys and Rooms.js
+++ Revamp/2024/graph/9-Keys and Rooms.js
@@ -29,3 +29,35 @@ const buildGraph = (rooms) => {
 
   return graph;
 };
+
+/**
+ * @param {number[][]} rooms
+ * @return {boolean}
+ */
+var canVisitAllRooms = function (rooms) {
+  const graph = buildGraph(rooms);
+  const visited = new Set();
+  const stack = [0];
+
+  while (stack.length > 0) {
+    const currentNode = stack.pop();
+    if (visited.has(currentNode)) continue;
+
+    visited.add(currentNode);
+
+    for (const neighbor of graph[currentNode]) {
+      stack.push(neighbor);
+    }
+  }
+
+  return visited.size === rooms.length;
+};
+
+const buildGraph2 = (rooms) => {
+  const graph = {};
+  for (let i = 0; i < rooms.length; i++) {
+    graph[i] = rooms[i];
+  }
+
+  return graph;
+};
