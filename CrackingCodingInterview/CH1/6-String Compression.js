c556981985bf7e90879f85ad4b33402b3484d7b5
eslamelkholy <eslamelkholy5325762@gmail.com>
2024-06-16 16:41:38 +0100

    String compression solution

---
+++ CrackingCodingInterview/CH1/6-String Compression.js
@@ -0,0 +1,29 @@
+/**
+ * @param {character[]} chars
+ * @return {number}
+ */
+var compress = function (chars) {
+  const freqMap = {};
+  let result = "";
+  let prevChar = chars[0];
+  let currentCount = 1;
+
+  for (let i = 1; i < chars.length; i++) {
+    const currentChar = chars[i];
+
+    if (currentChar === prevChar) {
+      currentCount++;
+    }
+
+    if (currentChar !== prevChar || i === chars.length - 1) {
+      result += prevChar + currentCount;
+      prevChar = currentChar;
+      currentCount = 1;
+    }
+  }
+
+  console.log(result);
+};
+
+console.log(compress((chars = ["a", "a", "b", "b", "c", "c", "c"])));
+console.log(compress((chars = ["a", "a", "b", "c", "c", "c", "c", "c", "a", "a", "a"])));
