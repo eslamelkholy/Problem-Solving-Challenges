1dc45d37f14b5dfa10ccd092d9b9756b93671267
eslamelkholy <eslamelkholy5325762@gmail.com>
2024-06-16 17:50:03 +0100

    Rotate image solution without extra space

--- CrackingCodingInterview/CH1/7-Rotate Image.js
+++ CrackingCodingInterview/CH1/7-Rotate Image.js
@@ -27,9 +27,9 @@ var rotate = function (matrix) {
 
   for (var i = 0; i < Math.floor((n + 1) / 2); i++) {
     for (var j = 0; j < Math.floor(n / 2); j++) {
-      const temp = matrix[n - 1 - j][i];
-      matrix[n - 1 - j][i] = matrix[n - 1 - i][n - j - 1];
-      matrix[n - 1 - i][n - j - 1] = matrix[j][n - 1 - i];
+      const temp = matrix[n - j - 1][i];
+      matrix[n - j - 1][i] = matrix[n - 1 - i][n - 1 - j];
+      matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i];
       matrix[j][n - 1 - i] = matrix[i][j];
       matrix[i][j] = temp;
     }
