ea7ab0e07c954494b62f9c6c143dfae717b0c73f
eslamelkholy <eslamelkholy5325762@gmail.com>
2024-06-16 17:46:32 +0100

    Rotate image optimized solution

--- CrackingCodingInterview/CH1/7-Rotate Image.js
+++ CrackingCodingInterview/CH1/7-Rotate Image.js
@@ -18,4 +18,22 @@ var rotate = function (matrix) {
   return matrix;
 };
 
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var rotate = function (matrix) {
+  let n = matrix.length;
+
+  for (var i = 0; i < Math.floor((n + 1) / 2); i++) {
+    for (var j = 0; j < Math.floor(n / 2); j++) {
+      const temp = matrix[n - 1 - j][i];
+      matrix[n - 1 - j][i] = matrix[n - 1 - i][n - j - 1];
+      matrix[n - 1 - i][n - j - 1] = matrix[j][n - 1 - i];
+      matrix[j][n - 1 - i] = matrix[i][j];
+      matrix[i][j] = temp;
+    }
+  }
+};
+
 // Leetcode: https://leetcode.com/problems/rotate-image/
