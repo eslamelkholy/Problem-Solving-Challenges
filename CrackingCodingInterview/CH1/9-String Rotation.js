c3174dbc5652a72fd1d95944b0b0012788a3056f
eslamelkholy <eslamelkholy5325762@gmail.com>
2024-06-17 14:42:11 +0100

    String Rotation solution

---
+++ CrackingCodingInterview/CH1/9-String Rotation.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} s
+ * @param {string} goal
+ * @return {boolean}
+ */
+var rotateString = function (s, goal) {
+  if (s.length !== goal.length) return false;
+
+  return (s + s).includes(goal);
+};
+
+console.log("waterbottle", "erbottlewat");
+
+// https://leetcode.com/problems/rotate-string/
