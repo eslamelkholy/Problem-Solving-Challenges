7ca8a11dcefdfe454dc028f81241e3bf1b1801f7
eslamelkholy <eslamelkholy5325762@gmail.com>
2024-06-18 23:00:28 +0100

    Delete middle node using fast and slow pointer

--- CrackingCodingInterview/CH2 - Linkedlist/3-Delete the Middle Node of a Linked List.js
+++ CrackingCodingInterview/CH2 - Linkedlist/3-Delete the Middle Node of a Linked List.js
@@ -30,3 +30,29 @@ var deleteMiddle = function (head) {
 
   return prev.next;
 };
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteMiddle = function (head) {
+  if (!head.next) return null;
+
+  let slow = head;
+  let fast = head.next.next;
+
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+
+  slow.next = slow.next.next;
+  return head;
+};
