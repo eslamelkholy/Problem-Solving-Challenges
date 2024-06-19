a5b4181874046824649a4c41aa33868b36236c00
eslamelkholy <eslamelkholy5325762@gmail.com>
2024-06-19 14:21:37 +0100

    Add two numbers using some math

--- CrackingCodingInterview/CH2 - Linkedlist/5-Add Two Numbers.js
+++ CrackingCodingInterview/CH2 - Linkedlist/5-Add Two Numbers.js
@@ -34,3 +34,40 @@ var addTwoNumbers = function (l1, l2) {
 
   return prev.next;
 };
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function (l1, l2) {
+  let carry = 0;
+  let head = new ListNode(0);
+  let prev = head;
+
+  // So keep track of both lists and the carry
+  while (l1 || l2 || carry !== 0) {
+    let sum1 = l1 ? l1.val : 0;
+    let sum2 = l2 ? l2.val : 0;
+    const totalSum = sum1 + sum2 + carry;
+
+    carry = Math.floor(totalSum / 10); // to get the rest
+
+    const val = totalSum % 10; // Get second digit of number
+
+    head.next = new ListNode(val);
+    head = head.next;
+
+    if (l1) l1 = l1.next;
+    if (l2) l2 = l2.next;
+  }
+
+  return prev.next;
+};
