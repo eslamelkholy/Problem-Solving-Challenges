9a03a1320dd139ce7fa526df677794fd8145b71a
eslamelkholy <eslamelkholy5325762@gmail.com>
2024-06-29 22:24:23 +0100

    Implement queue using stack

---
+++ CrackingCodingInterview/CH3 - Stacks and Queues/5-Implement Queue using Stacks.js
@@ -0,0 +1,42 @@
+class MyQueue {
+  constructor() {
+    this.stack1 = [];
+  }
+  /**
+   * @param {number} x
+   * @return {void}
+   */
+  push(x) {
+    this.stack.push(x);
+  }
+  /**
+   * @return {number}
+   */
+  pop() {
+    const element = this.stack[0];
+    this.stack.splice(0, 1);
+
+    return element;
+  }
+  /**
+   * @return {number}
+   */
+  peek() {
+    return this.stack[0];
+  }
+  /**
+   * @return {boolean}
+   */
+  empty() {
+    return this.stack.length === 0;
+  }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * var obj = new MyQueue()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.peek()
+ * var param_4 = obj.empty()
+ */
