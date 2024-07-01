361c0f807a0970306e6533761bcd38d2786fa614
eslamelkholy <eslamelkholy5325762@gmail.com>
2024-07-01 19:47:39 +0100

    Three consecutive Odds

---
+++ leetcodeDailyChallenge/2024/July/1-Three Consecutive Odds.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var threeConsecutiveOdds = function (arr) {
+  for (let i = 0; i < arr.length - 2; i++) {
+    if (arr[i] % 2 === 0) continue;
+
+    for (j = i + 1; j <= i + 2; j++) {
+      if (arr[j] % 2 === 0) {
+        j = i + 3;
+      }
+      if (j === i + 2) return true;
+    }
+  }
+
+  return false;
+};
