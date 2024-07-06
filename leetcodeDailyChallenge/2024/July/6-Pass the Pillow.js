c3aabc6a0390b62f49a29883715021adae3918ae
eslamelkholy <eslamelkholy5325762@gmail.com>
2024-07-06 14:36:36 +0100

    Pass the pillow using maths

--- leetcodeDailyChallenge/2024/July/6-Pass the Pillow.js
+++ leetcodeDailyChallenge/2024/July/6-Pass the Pillow.js
@@ -17,3 +17,19 @@ var passThePillow = function (n, time) {
 
   return running;
 };
+
+/**
+ * @param {number} n
+ * @param {number} time
+ * @return {number}
+ */
+var passThePillow = function (n, time) {
+  const fullRounds = Math.floor(time / (n - 1));
+  let remainingRunnings = time % (n - 1);
+
+  if (fullRounds % 2 === 0) {
+    return remainingRunnings + 1;
+  }
+
+  return n - remainingRunnings;
+};
