4712581a8041626feac60aab3e3c92e8b9f064ce
eslamelkholy <eslamelkholy5325762@gmail.com>
2024-07-15 15:23:06 +0100

    Path sum ||| added more solutions

--- CrackingCodingInterview/CH4 - Tree and Graphs/12-Path Sum III.js
+++ CrackingCodingInterview/CH4 - Tree and Graphs/12-Path Sum III.js
@@ -12,6 +12,43 @@
  * @return {number}
  */
 var pathSum = function (root, targetSum) {
+  if (!root) return 0;
+
+  const countPaths = (root, currentSum) => {
+    if (!root) return 0;
+
+    currentSum += root.val;
+    let pathsCount = 0;
+
+    if (currentSum === targetSum) pathsCount++;
+
+    pathsCount += countPaths(root.left, currentSum);
+    pathsCount += countPaths(root.right, currentSum);
+
+    return pathsCount;
+  };
+
+  const rootPaths = countPaths(root, 0);
+  const leftPaths = pathSum(root.left, targetSum);
+  const rightPaths = pathSum(root.right, targetSum);
+
+  return rootPaths + leftPaths + rightPaths;
+};
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {number}
+ */
+var pathSum = function (root, targetSum) {
   const prefixSumMap = {};
   let targetSumCount = 0;
 
