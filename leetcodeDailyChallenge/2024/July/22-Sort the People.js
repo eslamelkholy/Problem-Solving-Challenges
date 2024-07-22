62895f0be0293454d55c4a623e7809f30a55b929
eslamelkholy <eslamelkholy5325762@gmail.com>
2024-07-22 12:19:32 +0100

    Sort the people using map and sorting

--- leetcodeDailyChallenge/2024/July/22-Sort the People.js
+++ leetcodeDailyChallenge/2024/July/22-Sort the People.js
@@ -18,3 +18,24 @@ var sortPeople = function (names, heights) {
 
   return names;
 };
+
+/**
+ * @param {string[]} names
+ * @param {number[]} heights
+ * @return {string[]}
+ */
+var sortPeople = function (names, heights) {
+  let heightsMap = {};
+
+  for (let i = 0; i < names.length; i++) {
+    heightsMap[heights[i]] = names[i];
+  }
+
+  heights.sort((a, b) => b - a);
+
+  for (let i = 0; i < names.length; i++) {
+    names[i] = heightsMap[heights[i]];
+  }
+
+  return names;
+};
