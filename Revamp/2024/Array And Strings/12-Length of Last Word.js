24a71e4cb7d7f92cedd4c3dd8dea426b8b49ded0
eslamelkholy <eslamelkholy5325762@gmail.com>
2024-08-01 23:28:01 +0100

    Length of last word

--- Revamp/2024/Array And Strings/12-Length of Last Word.js
+++ Revamp/2024/Array And Strings/12-Length of Last Word.js
@@ -18,3 +18,21 @@ var lengthOfLastWord = function (s) {
 
   return result;
 };
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function (s) {
+  let result = 0;
+
+  for (let i = s.length - 1; i >= 0; i--) {
+    if (s[i] === " " && result !== 0) {
+      return result;
+    }
+
+    if (s[i] !== " ") result++;
+  }
+
+  return result;
+};
