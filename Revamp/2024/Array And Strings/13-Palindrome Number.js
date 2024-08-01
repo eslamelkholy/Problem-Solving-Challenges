ddbf92c54788f24e22f217a5d1a0d58360fe1789
eslamelkholy <eslamelkholy5325762@gmail.com>
2024-08-01 23:29:22 +0100

    Palindrom number

--- Revamp/2024/Array And Strings/13-Palindrome Number.js
+++ Revamp/2024/Array And Strings/13-Palindrome Number.js
@@ -5,7 +5,7 @@
 var isPalindrome = function (x) {
   const str = String(x);
 
-  for (let i = 0; i < str.length; i++) {
+  for (let i = 0; i < str.length / 2; i++) {
     if (str[i] !== str[str.length - 1 - i]) return false;
   }
 
