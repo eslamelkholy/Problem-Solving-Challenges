3c302c48a08a7110c3f97763f266e68e7bb79731
eslamelkholy <eslamelkholy5325762@gmail.com>
2024-08-01 18:10:41 +0100

    Added straightforward solution O(N LOG N)

--- Revamp/2024/Array And Strings/1-Merge Sorted Array.js
+++ Revamp/2024/Array And Strings/1-Merge Sorted Array.js
@@ -6,6 +6,21 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function (nums1, m, nums2, n) {
+  for (let i = 0; i < n; i++) {
+    nums1[m + i] = nums2[i];
+  }
+
+  nums1.sort((a, b) => a - b);
+};
+
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function (nums1, m, nums2, n) {
   let left = m - 1;
   let right = n - 1;
 
