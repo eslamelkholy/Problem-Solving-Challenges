1007f6a50f382100ab280cf56044327fbf18b75f
eslamelkholy <eslamelkholy5325762@gmail.com>
2024-08-03 23:05:25 +0100

    Is same tree solution using dfs

--- Revamp/2024/binaryTree/13-Same Tree.js
+++ Revamp/2024/binaryTree/13-Same Tree.js
@@ -33,3 +33,30 @@ var isSameTree = function (p, q) {
 
   return true;
 };
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+var isSameTree = function (p, q) {
+  const dfs = (root1, root2) => {
+    if (!root1 && !root2) return true;
+
+    if (!root1 || !root2) return false;
+
+    if (root1.val !== root2.val) return false;
+
+    return dfs(root1.left, root2.left) && dfs(root1.right, root2.right);
+  };
+
+  return dfs(p, q);
+};
