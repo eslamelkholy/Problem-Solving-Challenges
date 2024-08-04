9adfb7d0be031b43246dd616ef16fdc4faefc50b
eslamelkholy <eslamelkholy5325762@gmail.com>
2024-08-04 14:01:52 +0100

    Optimize search in 2d matrix algo

--- Revamp/2024/Binar Search/3-Search a 2D Matrix.js
+++ Revamp/2024/Binar Search/3-Search a 2D Matrix.js
@@ -4,24 +4,26 @@
  * @return {boolean}
  */
 var searchMatrix = function (matrix, target) {
-  for (const arr of matrix) {
-    if (binarySearch(arr, target)) return true;
-    if (arr[0] > target) return false;
+  for (const nums of matrix) {
+    if (nums[nums.length - 1] < target) continue;
+    if (nums[0] > target) return false;
+
+    if (binarySearch(nums, target)) return true;
   }
 
   return false;
 };
 
-const binarySearch = (arr, target) => {
+const binarySearch = (nums, target) => {
   let left = 0;
-  let right = arr.length - 1;
+  let right = nums.length - 1;
 
   while (left <= right) {
     const mid = Math.floor((left + right) / 2);
 
-    if (arr[mid] === target) return true;
+    if (nums[mid] === target) return true;
 
-    if (arr[mid] < target) {
+    if (nums[mid] < target) {
       left = mid + 1;
     } else {
       right = mid - 1;
