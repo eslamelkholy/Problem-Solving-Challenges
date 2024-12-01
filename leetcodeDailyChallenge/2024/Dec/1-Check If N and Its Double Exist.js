961f887db4b6006bcaef658b60408e18914e429d
eslamelkholy <eslamelkholy5325762@gmail.com>
2024-12-01 12:22:11 +0000

    Check if N exists using Map O(2N) And set O(N)

--- leetcodeDailyChallenge/2024/Dec/1-Check If N and Its Double Exist.js
+++ leetcodeDailyChallenge/2024/Dec/1-Check If N and Its Double Exist.js
@@ -11,3 +11,44 @@ var checkIfExist = function (arr) {
 
   return false;
 };
+
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var checkIfExist = function (arr) {
+  const numMap = {};
+  for (let i = 0; i < arr.length; i++) {
+    numMap[arr[i]] = i;
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    const num = arr[i];
+    if (num % 2 === 0) {
+      const dividedNum = num / 2;
+      if (numMap[dividedNum] !== undefined && i !== numMap[dividedNum]) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+};
+
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var checkIfExist = function (arr) {
+  const seen = new Set();
+
+  for (const num of arr) {
+    if (seen.has(num * 2) || (num % 2 === 0 && seen.has(num / 2))) {
+      return true;
+    }
+
+    seen.add(num);
+  }
+
+  return false;
+};
