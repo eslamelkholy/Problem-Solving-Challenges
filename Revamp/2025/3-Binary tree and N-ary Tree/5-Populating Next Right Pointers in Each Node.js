0523a712c986d67ff9a570e8300142bf0565216c
eslamelkholy <eslamelkholy5325762@gmail.com>
2025-11-11 21:08:46 +0000

    populating next pointer

---
+++ Revamp/2025/3-Binary tree and N-ary Tree/5-Populating Next Right Pointers in Each Node.js
@@ -0,0 +1,36 @@
+/**
+ * // Definition for a _Node.
+ * function _Node(val, left, right, next) {
+ *    this.val = val === undefined ? null : val;
+ *    this.left = left === undefined ? null : left;
+ *    this.right = right === undefined ? null : right;
+ *    this.next = next === undefined ? null : next;
+ * };
+ */
+
+/**
+ * @param {_Node} root
+ * @return {_Node}
+ */
+var connect = function (root) {
+  const queue = [root];
+  if (!root) return root;
+
+  while (queue.length > 0) {
+    const N = queue.length;
+    for (let i = 0; i < N; i++) {
+      const currentNode = queue.shift();
+
+      if (i === N - 1) {
+        queue.next = null;
+      } else {
+        currentNode.next = queue[0];
+      }
+
+      if (currentNode.left) queue.push(currentNode.left);
+      if (currentNode.right) queue.push(currentNode.right);
+    }
+  }
+
+  return root;
+};
