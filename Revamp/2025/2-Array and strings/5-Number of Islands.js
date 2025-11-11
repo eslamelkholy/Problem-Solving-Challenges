092cbd094a999e13da1f75887074df89b49f2d16
eslamelkholy <eslamelkholy5325762@gmail.com>
2025-11-11 22:08:32 +0000

    number of islands

---
+++ Revamp/2025/2-Array and strings/5-Number of Islands.js
@@ -0,0 +1,35 @@
+/**
+ * @param {character[][]} grid
+ * @return {number}
+ */
+var numIslands = function (grid) {
+  let islands = 0;
+
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[0].length; j++) {
+      islands += dfs(grid, i, j);
+    }
+  }
+
+  return islands;
+};
+
+const dfs = (grid, x, y) => {
+  if (grid[x] === undefined || grid[x][y] === undefined || grid[x][y] === '0')
+    return 0;
+
+  grid[x][y] = '0'; // mark as visited
+
+  for (const [dx, dy] of DIRS) {
+    dfs(grid, x + dx, y + dy);
+  }
+
+  return 1;
+};
+
+const DIRS = [
+  [1, 0],
+  [-1, 0],
+  [0, 1],
+  [0, -1],
+];
