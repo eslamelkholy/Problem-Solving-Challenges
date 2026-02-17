ae71335cf8b99bd386225913271f2d4b585cabd5
eslamelkholy <eslamelkholy5325762@gmail.com>
2026-02-17 21:11:31 +0000

    Minimum Size Subarray sum

---
+++ Revamp/2026/4-Sliding window/2- Minimum Size Subarray Sum.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number} target
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minSubArrayLen = function (target, nums) {
+  let left = 0;
+  let right = 0;
+  let currentSum = 0;
+  let minSize = Number.MAX_SAFE_INTEGER;
+
+  while (right < nums.length) {
+    currentSum += nums[right];
+
+    while (currentSum >= target) {
+      minSize = Math.min(minSize, right - left + 1);
+
+      currentSum -= nums[left];
+      left++;
+    }
+
+    right++;
+  }
+
+  return minSize === Number.MAX_SAFE_INTEGER ? 0 : minSize;
+};
